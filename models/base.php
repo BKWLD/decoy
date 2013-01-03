ee9494f0d909204a6e7f6890f9348bc6df11ad27
Robert Reinhard <robert.reinhard@bkwld.com>
2013-01-03 12:30:15 -0800

    Letting the dev define an image() function on the model to return the image for the title()

--- models/base.php
+++ models/base.php
@@ -19,7 +19,8 @@ abstract class Decoy_Base_Model extends Eloquent {
 		$title = '';
 		
 		// Add a thumbnail to the title if there is an "image" field
-		if (isset($this->image)) $title .= '<img src="'.Croppa::url($this->image, 40, 40).'"/> ';
+		if (method_exists($this, 'image') && $this->image()) $title .= '<img src="'.Croppa::url($this->image(), 40, 40).'"/> ';
+		elseif (isset($this->image)) $title .= '<img src="'.Croppa::url($this->image, 40, 40).'"/> ';
 		
 		// Convert to an array so I can test for the presence of values.
 		// As an object, it would throw exceptions
