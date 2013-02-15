64ec3af80db8f6ea9d18cce5bea4f7f29db0e036
Robert Reinhard <robert.reinhard@bkwld.com>
2013-02-15 07:34:28 -0800

    Putting the old generic nav back

--- views/layouts/_nav.php
+++ views/layouts/_nav.php
@@ -1,52 +1,9 @@
 <? // FYI, this patial is populated from a view composer ?>
 
-
-<div class="user-bar">
-	<a class="mobile-menu" href="#">
-<!-- 		<div class="label">Menu</div>
- -->		<div class="icon"></div>
-	</a>
-	<div class="user-info">
-		<div class="notify"><a href="#"></a></div>
-<!-- 		<div class="user"><img src="/img/tmp/user-small.jpg" alt="">
-			<div class="wrap"><div class="inner">Mister Username</div></div>
-		</div>
- -->
-		<ul class="nav pull-right user">
-			<li class="dropdown">
-				<a class="user-dropdown" href="#" class="dropdown-toggle" data-toggle="dropdown">
-					<span>Hi, <?=Decoy_Auth::user_name()?>!</span>
-					<img src="<?=Decoy_Auth::user_photo()?>" class="gravatar"/>
-					<b class="caret"></b>
-				</a>
-				<ul class="dropdown-menu">
-					<? if (is_a(new Decoy_Auth, 'Decoy\Auth')): ?>
-						<li><a href="<?=action('decoy::admins')?>">Admins</a></li>
-						<li class="divider"></li>
-					<? endif ?>
-					<? if (Decoy_Auth::developer()): ?>
-						<li><a href="<?=action('decoy::tasks')?>">Tasks</a></li>
-						<li class="divider"></li>
-					<? endif ?>
-					<li><a href="<?=Decoy_Auth::user_url()?>">Account</a></li>
-					<li><a href="<?=Decoy_Auth::logout_url()?>">Log out</a></li>
-				</ul>
-			</li>
-		</ul>
-
-
-	</div>
-	<div class="edit-nav">
-<!-- 		<div class="col"><a class="dashboard" href="#">Dashboard</a></div>
- -->		<div class="col"><a class="edit-profile" href="#">Edit Profile</a></div>
-	</div>
-</div>
-
-<div id="admin-navbar" class="navbar navbar-inverse " data-js-view="navbar">
+<div class="navbar navbar-inverse navbar-fixed-top" data-js-view="navbar">
 	<div class="navbar-inner">
-
 		<div class="container">
-
+			
 			<?// This is the button to expand on mobile ?>
 			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
 				<span class="icon-bar"></span>
@@ -61,14 +18,14 @@
 				<? if (Decoy_Auth::check()): ?>
 					
 					<?// The menu ?>
-					<ul class="nav admin-nav">
+					<ul class="nav">
 						<? foreach($pages as $page): ?>
 							
 							<?// Dropdown menu?>
 							<? if (!empty($page->children)): ?>
 								<li class="dropdown <?=$page->active?'active':null?>">
 									<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?=$page->label?> <b class="caret"></b></a>
-									<ul class="dropdown-menu" id="nested">
+									<ul class="dropdown-menu">
 										
 										<?// Loop through children ?>
 										<? foreach($page->children as $child): ?>
@@ -89,7 +46,26 @@
 						<? endforeach ?>
 					</ul>
 					
-					
+					<ul class="nav pull-right">
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+								<span>Hi, <?=Decoy_Auth::user_name()?>!</span>
+								<img src="<?=Decoy_Auth::user_photo()?>" class="gravatar"/>
+								<b class="caret"></b>
+							</a>
+							<ul class="dropdown-menu">
+								<? if (is_a(new Decoy_Auth, 'Decoy\Auth')): ?>
+									<li><a href="<?=action('decoy::admins')?>">Admins</a></li>
+									<li class="divider"></li>
+								<? endif ?>
+								<? if (Decoy_Auth::developer()): ?>
+									<li><a href="<?=action('decoy::tasks')?>">Tasks</a></li>
+									<li class="divider"></li>
+								<? endif ?>
+								<li><a href="<?=Decoy_Auth::user_url()?>">Account</a></li>
+								<li><a href="<?=Decoy_Auth::logout_url()?>">Log out</a></li>
+							</ul>
+						</ul>
 					
 				<? endif ?>
 			</div>
