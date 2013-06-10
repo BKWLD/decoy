e19f49eb61eb0e704ab985934eb8dba32431b73e
Robert Reinhard <robert.reinhard@bkwld.com>
2013-06-10 10:06:43 -0700

    Updating path to stylesheet to match Camo conventions

--- src/views/layouts/buk_builder/_header.php
+++ src/views/layouts/buk_builder/_header.php
@@ -1,2 +1,2 @@
-<link data-build-id="admin-style" rel="stylesheet" href="/css/admin/app.css"/>
+<link data-build-id="admin-style" rel="stylesheet" href="/css/admin/style.css"/>
 <script data-build-id="modernizr" src="/js/libs/modernizr.js"></script>
