9d6811f6bc7eb2f6ecd6cd65b3edb557b9087282
Robert Reinhard <robert.reinhard@bkwld.com>
2013-07-08 16:02:13 -0700

    Should allow 0s or NULLs for the hidden visiblity

--- src/views/shared/form/display/_visible.php
+++ src/views/shared/form/display/_visible.php
@@ -1,8 +1,8 @@
 <?// Just the visbibility field for the display module?>
 
 <?= Former::radios('visible', 'Availability')->inline()->radios(array(
-	'Visible' => array('value' => 1, 'checked' => true),
-	'Hidden' => array('value' => 0),
-))->blockHelp(($url = $item->deepLink()) ? 
+	'Visible' => array('value' => '1', 'checked' => empty($item) ? true : $item->visible == 1),
+	'Hidden' => array('value' => '', 'checked' => empty($item) ? false : $item->visible != 1),
+))->blockHelp((!empty($item) && $url = $item->deepLink()) ? 
 	'If hidden, this content will be accessible via <a href="'.$url.'">deep link</a> but not in lists' : 
 	'If hidden, this content will be completely inaccessible') ?>
