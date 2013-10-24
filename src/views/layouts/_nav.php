2cfa3ba8bf865ee27b05a7b7d78779a290795155
Robert Reinhard <robert.reinhard@bkwld.com>
2013-10-24 15:36:37 -0700

    Styling the color of the navbar
    Wanted some new freshness since i'm not doing #173

--- src/views/layouts/_nav.php
+++ src/views/layouts/_nav.php
@@ -1,6 +1,6 @@
 <? // FYI, this patial is populated from a view composer ?>
 
-<div class="navbar navbar-inverse navbar-fixed-top">
+<div class="navbar navbar-fixed-top">
 	<div class="navbar-inner">
 		<div class="container">
 			
