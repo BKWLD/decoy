4e20702fc3297080bb65ae0a137466f0e5921cd4
Robert Reinhard <robert.reinhard@bkwld.com>
2014-02-19 16:17:03 -0800

    Fixing it so an empty related array still triggers

--- src/views/shared/form/display/_slug.php
+++ src/views/shared/form/display/_slug.php
@@ -4,7 +4,7 @@
 $url_link = 'deep link';
 if (!empty($item->slug)) {
 	$prepend = '/';
-	$span = empty($related) ? 'span7' : 'span4'; // If related is set, this is on a 2 columned form
+	$span = isset($related) ? 'span4' : 'span7'; // If related is set, this is on a 2 columned form
 	if ($url = $item->deepLink()) {
 		$url_link = '<a href="'.$url.'">deep link</a>';
 		$prepend = preg_replace('#/[\w-]+$#', '/', parse_url($url, PHP_URL_PATH));
