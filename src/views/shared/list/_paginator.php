81beb6463b65191e741f30846e8d8fbe768b1e34
Robert Reinhard <robert.reinhard@bkwld.com>
2014-03-06 11:56:13 -0800

    Only show paginator if it's needed

--- src/views/shared/list/_paginator.php
+++ src/views/shared/list/_paginator.php
@@ -4,32 +4,34 @@
  * This is implemented by the _pagination partial and hydrated
  * by the Bkwld\Decoy\Html\Paginator class to render pagination.
  * It is based on /laravel/framework/src/Illuminate/Pagination/views/slider.php
+ * The paginator is only shown if there are more results than the smallest
+ * per_page option.
  */
 
-$presenter = new Illuminate\Pagination\BootstrapPresenter($paginator); ?>
-<div class="pagination">
-
-	<?// The list of pages ?>
-	<? if ($paginator->getLastPage() > 1): ?>
-		<ul>
-			<?=$presenter->render(); ?>
+$presenter = new Illuminate\Pagination\BootstrapPresenter($paginator); 
+if ($paginator->getTotal() > Bkwld\Decoy\Controllers\Base::PER_PAGE): ?>
+	<div class="pagination">
+	
+		<?// The list of pages ?>
+		<? if ($paginator->getLastPage() > 1): ?>
+			<ul>
+				<?=$presenter->render(); ?>
+			</ul>
+		<? endif ?>
+	
+		<?// Per page selector
+		$options = array(Bkwld\Decoy\Controllers\Base::PER_PAGE, 40, 'all');
+		$count = Input::get('count', $options[0]); ?>
+		<ul class="per-page">
+			<li class="disabled"><span>Show</span></li>
+			<? foreach($options as $option): ?>
+				<? if ($count == $option): ?>
+					<?=$presenter->getActivePageWrapper(ucfirst($option))?>
+				<? else: ?>
+					<?=$presenter->getPageLinkWrapper($paginator->addQuery('count', $option)->getUrl(1), ucfirst($option))?>
+				<? endif ?>
+			<? endforeach ?>
+	
 		</ul>
-	<? endif ?>
-
-	<?// Per page selector
-	$options = array(Bkwld\Decoy\Controllers\Base::PER_PAGE, 40, 'all');
-	$count = Input::get('count', $options[0]);
-	?>
-	<ul class="per-page">
-
-		<li class="disabled"><span>Show</span></li>
-		<? foreach($options as $option): ?>
-			<? if ($count == $option): ?>
-				<?=$presenter->getActivePageWrapper(ucfirst($option))?>
-			<? else: ?>
-				<?=$presenter->getPageLinkWrapper($paginator->addQuery('count', $option)->getUrl(1), ucfirst($option))?>
-			<? endif ?>
-		<? endforeach ?>
-
-	</ul>
-</div>
+	</div>
+<? endif ?>
