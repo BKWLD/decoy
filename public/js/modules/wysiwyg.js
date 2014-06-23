c40f0ad158497eecd85aee77383b3d053b73417a
Robert Reinhard <robert.reinhard@bkwld.com>
2014-06-23 11:26:50 -0700

    Apply width and height on imgs in CKEditor as tag attributes instead of inline styles
    Fixes #229

--- public/js/modules/wysiwyg.js
+++ public/js/modules/wysiwyg.js
@@ -29,6 +29,47 @@ define(function(require) {
 			{ name: 'source', items : [ 'Source' ] }
 		]
 	};
+
+	// For uploaded images, don't specify the width and height as inline styles.  Instead,
+	// make the attributes on the img tag.
+	// http://stackoverflow.com/a/6056896/59160
+	CKEDITOR.on('instanceReady', function (ev) {
+		ev.editor.dataProcessor.htmlFilter.addRules({
+			elements: {
+				$: function (element) {
+					// Output dimensions of images as width and height
+					if (element.name == 'img') {
+						var style = element.attributes.style;
+
+						if (style) {
+							// Get the width from the style.
+							var match = /(?:^|\s)width\s*:\s*(\d+)px/i.exec(style),
+								width = match && match[1];
+
+							// Get the height from the style.
+							match = /(?:^|\s)height\s*:\s*(\d+)px/i.exec(style);
+							var height = match && match[1];
+
+							if (width) {
+								element.attributes.style = element.attributes.style.replace(/(?:^|\s)width\s*:\s*(\d+)px;?/i, '');
+								element.attributes.width = width;
+							}
+
+							if (height) {
+								element.attributes.style = element.attributes.style.replace(/(?:^|\s)height\s*:\s*(\d+)px;?/i, '');
+								element.attributes.height = height;
+							}
+						}
+					}
+
+					if (!element.attributes.style)
+						delete element.attributes.style;
+
+					return element;
+				}
+			}
+		});
+	});
 	
 	// Enable CKFinder
 	var allow_uploads = false;
