14a48a0e92faa0fedd0b87272c1114eb24e8bc44
Robert Reinhard <robert.reinhard@bkwld.com>
2014-08-02 13:17:32 -0700

    Removing deprecated code related to Decoy\Fields\*
    #194

--- src/Bkwld/Decoy/Input/ManyToManyChecklist.php
+++ src/Bkwld/Decoy/Input/ManyToManyChecklist.php
@@ -16,61 +16,6 @@ class ManyToManyChecklist {
 	const PREFIX = '_many_to_many_';
 
 	/**
-	 * Render the form element
-	 * ex: <?= Decoy::manyToManyChecklist($__data, 'events', array('blockHelp' => 'Blah blah')) ?>
-	 * @param array $__data A The data passed to the view, to get at the item
-	 * @param string $relationship The name of the relationship function on the model
-	 * @param array $options Former key-value pairs, where the key is the function name.  Also, you
-	 *                       append onto the query with a key of "query" and a callback for the value.
-	 * @return string HTML
-	 */
-	public function render($__data, $relationship, $options = array()) {
-
-		// Get all the related items to THE model instance passed to the view
-		if (isset($__data['item'])) $joined = $__data['item']->$relationship;
-
-		// This element will get a special name that will get handled specially by the
-		// base controller. I think Laravel won't try to save fields that begin with
-		// an "_".
-		$name = self::PREFIX.$relationship;
-
-		// Get the full list of items
-		$query = call_user_func(ucfirst(Str::singular($relationship)).'::ordered');
-		if (!empty($options['query']) && is_callable($options['query'])) {
-			call_user_func($options['query'], $query);
-			unset($options['query']);
-		}
-
-		// Create the data that Former expects
-		$boxes = array();
-		foreach($query->get() as $row) {
-
-			// Create the row
-			$ar = array('value' => $row->getKey(), 'name' => $name.'[]');
-
-			// Check if it should be checked
-			if (isset($joined) && $joined->contains($row->getKey())) $ar['checked'] = true;
-
-			// Add it.  The str_replace fixes Former's auto conversion of underscores
-			// into spaces.
-			$boxes[str_replace('_', '&#95;', '<a href="/admin/'.Str::snake($relationship,'-').'/'.$row->getKey().'/edit">'.$row->title().'</a>')] = $ar;
-		}
-
-		// Render an empty message if no boxes created
-		if (empty($boxes)) {
-			$label = String::titleFromKey($relationship);
-			return Former::note($label,
-				'<i class="icon-info-sign"></i> You have not <a href="/admin/'.Str::snake($relationship,'-').'">created</a> any <b>'.$label.'</b>.');
-		}
-		
-		// Create the form element, applying any extra configuration options
-		$el = Former::checkbox($relationship)->checkboxes($boxes)->addGroupClass('many-to-many-checklist');
-		foreach($options as $func => $args) $el = call_user_func_array(array($el, $func), (array) $args);
-		return $el;
-
-	}
-
-	/**
 	 * Take input from a Many to Many Checklist and commit it to the db
 	 * @param Bkwld\Decoy\Models\Base $model A model instance
 	 */
