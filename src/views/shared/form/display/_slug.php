b3f15d900f9ad73e27f183415316c38edfe5028a
Robert Reinhard <robert.reinhard@bkwld.com>
2014-09-08 16:36:23 -0700

    Fixing styling of slug field

--- src/views/shared/form/display/_slug.php
+++ src/views/shared/form/display/_slug.php
@@ -4,11 +4,10 @@
 $url_link = 'deep link';
 if (!empty($item->slug)) {
 	$prepend = '/';
-	$span = isset($related) ? 'span4' : 'span7'; // If related is set, this is on a 2 columned form
 	if ($url = $item->deepLink()) {
 		$url_link = '<a href="'.$url.'">deep link</a>';
 		$prepend = preg_replace('#/[\w-]+$#', '/', parse_url($url, PHP_URL_PATH));
 	}
-	echo Former::text('slug')->blockHelp('Used to form the '.$url_link.' to this content')->prepend($prepend)->class($span);
+	echo Former::text('slug')->blockHelp('Used to form the '.$url_link.' to this content')->prepend($prepend);
 }
 ?>
