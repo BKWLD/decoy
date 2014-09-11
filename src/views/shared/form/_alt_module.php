10278653b3f2105bd4f29883355705af715b2cbc
Robert Reinhard <robert.reinhard@bkwld.com>
2014-09-11 10:10:16 -0700

    Removing some legacy views

--- src/views/shared/form/_alt_module.php
+++
@@ -1,8 +0,0 @@
-<?// Adds alt tags to images?>
-
-<legend>SEO</legend>
-
-<?
-	$span = empty($related) ? 'span9' : 'span6';
-	echo Former::text('alt', '"Alt" attribute')->class($span)->blockHelp('Content for the image tag "alt" attribute');
-?>
