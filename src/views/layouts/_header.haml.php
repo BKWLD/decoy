e75bc98b9b39e6bf87edde9e7f0d47ad65738be0
Matthew Aebersold <matt.aebersold@bkwld.com>
2014-10-17 12:41:18 -0700

    Getting the rough nav and user info in place

--- src/views/layouts/_header.haml.php
+++ src/views/layouts/_header.haml.php
@@ -1,4 +1,43 @@
+-$auth = App::make('decoy.auth')
 .header
 	.inner
 		%h1 !=Config::get('decoy::site_name')
-	
+
+		-# The account menu
+		%ul.user
+			%li.dropdown
+				
+				-# Dropdown menu
+				%a.dropdown-toggle(data-toggle='dropdown')
+					%span Hi, #{$auth->userName()}
+					%img.gravatar(src=$auth->userPhoto())
+					%span.caret
+		
+				-# Options
+				%ul.dropdown-menu
+		
+					-if(is_a($auth, 'Bkwld\Decoy\Auth\Sentry') && $auth->can('read', 'admins'))
+						%li
+							%a(href=DecoyURL::action('Bkwld\\Decoy\\Controllers\\Admins@index')) Admins
+						%li
+							%a(href=$auth->userUrl()) Your account
+						%li.divider
+		
+					-$divider = false
+					-if($auth->developer())
+						-$divider = true
+						%li
+							%a(href=route('decoy\\commands')) Commands
+		
+					-if(count(Bkwld\Decoy\Models\Worker::all()))
+						-$divider = true
+						%li
+							%a(href=route('decoy\\workers')) Workers
+		
+					-if($divider)
+						%li.divider
+		
+					%li
+						%a(href='/') Public site
+					%li
+						%a(href=$auth->logoutUrl()) Log out
--- src/views/layouts/_nav.haml.php
+++ src/views/layouts/_nav.haml.php
@@ -4,6 +4,42 @@
 	.box.box-admin
 		%span ADMIN
 
+	%ul
+		-foreach($pages as $page)
+		
+			-if (!empty($page->children))
+		
+				-# Buffer the output so that it is only shown if children were added.  There
+				-# could be none if they were hidden by permissions rules
+				-ob_start()
+				-$child_added = false
+		
+				-# The pulldown
+				%li.dropdown(class=$page->active?'active':null)
+					%a.dropdown-toggle(href='#' data-toggle='dropdown')
+						!=$page->label
+						%span.caret
+		
+					-# The options
+					%ul.dropdown-menu(role="menu")
+						-foreach($page->children as $child)
+							-if (!empty($child->divider))
+								%li.divider
+							-elseif($auth->can('read', $child->url))
+								-$child_added = true
+								%li(class=$child->active?'active':null)
+									%a(href=$child->url)=$child->label
+		
+				-# Only show the dropdown if a child was added
+				-if ($child_added) 
+					-ob_end_flush()
+				-else 
+					-ob_end_clean()
+		
+			-else if($auth->can('read', $page->url))
+				%li(class=$page->active?'active':null)
+					%a(href=$page->url)=$page->label
+
 -#	.container
 -#
 -#		-# Brand and hamburger
