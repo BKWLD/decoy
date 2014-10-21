d70c1c86660cfde4eb0d45fd40c8ff742ab75d5f
Robert Reinhard <robert.reinhard@bkwld.com>
2014-10-21 13:34:45 -0700

    Getting search working
    Fix #275

--- src/views/shared/list/_search.php
+++ src/views/shared/list/_search.php
@@ -4,6 +4,6 @@
 <form class="form-inline search" data-js-view="search" data-schema='<?=json_encode($search)?>' data-title='<?=strtolower($title)?>' >
 	<div class="conditions">
 		<?// Most of the HTML is inserted by the backbone view ?>
-		<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span> Search</button>
+		<button type="submit" class="btn btn-sm outline"><span class="glyphicon glyphicon-search"></span> Search</button>
 	</div>
 </form>
