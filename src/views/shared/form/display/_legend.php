347605edd87f52ce36fc0ae6461691ce5fab787a
Eric Howard <eric.howard@bkwld.com>
2014-10-22 15:39:26 -0700

    outlined display button in legend #307

--- src/views/shared/form/display/_legend.php
+++ src/views/shared/form/display/_legend.php
@@ -3,6 +3,6 @@
 <div class="legend">
 		Display
 		<? if (!empty($item) && ($url = $item->deepLink())): ?>
-			<a href="<?=$url?>" class="btn btn-default btn-xs pull-right"><span class="glyphicon glyphicon-bookmark"></span> View</a>
+			<a href="<?=$url?>" class="btn btn-default btn-xs outline pull-right"><span class="glyphicon glyphicon-bookmark"></span> View</a>
 		<? endif ?>
 </div>
