6f19cc6077c1992735d394ee702d7b4ae09f9732
Eric Howard <eric.howard@bkwld.com>
2014-10-22 10:37:40 -0700

    wrapped tables in standard-list with headers #300

--- src/views/commands/index.php
+++ src/views/commands/index.php
@@ -1,27 +1,25 @@
-<h1>Commands
-	<small>Trigger any command for this site.  Note: these may take awhile to execute.</small>
-</h1>
-
 <div id="commands">
 	<? foreach($commands as $namespace => $subcommands): ?>
 		<div class='col-sm-6 col-lg-4'>
-			<legend><?=$namespace?></legend>
-			<table>
-				<? foreach($subcommands as $name => $command): ?>
-					<tr data-js-view="task-method">
-						<td>
-							<a href="<?=route('decoy\commands@execute', $command->getName())?>" class="btn btn-default">Execute</a>
-						</td>
-						<td>
-							<p>
-								<?=$name?>
-								<img src="/packages/bkwld/decoy/img/spinners/46x46.gif"/>
-							</p>
-							<p><small><?=$command->getDescription()?></small></p>
-						</td>
-					</tr>
-				 <? endforeach ?>
-			</table>
+			<div class="standard-list">
+				<div class="legend sidebar-header"><?=$namespace?></div>
+				<table class="">
+					<? foreach($subcommands as $name => $command): ?>
+						<tr data-js-view="task-method">
+							<td>
+								<a href="<?=route('decoy\commands@execute', $command->getName())?>" class="btn btn-default">Execute</a>
+							</td>
+							<td>
+								<p>
+									<?=$name?>
+									<img src="/packages/bkwld/decoy/img/spinners/46x46.gif"/>
+								</p>
+								<p><small><?=$command->getDescription()?></small></p>
+							</td>
+						</tr>
+					 <? endforeach ?>
+				</table>
+			</div>
 		</div>
 	<? endforeach ?>
 </div>
