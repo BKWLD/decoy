0f3a1d95e9873a87b233a9e3033c314bdd557bb7
Robert Reinhard <robert.reinhard@bkwld.com>
2014-10-24 12:23:00 -0700

    Make breadcrumbs always one line
    And adding hovers #278

--- src/views/layouts/_breadcrumbs.php
+++ src/views/layouts/_breadcrumbs.php
@@ -8,6 +8,15 @@ if (empty($breadcrumbs)) return;
 <div class="breadcrumbs affixable" data-top="0">
 	<?// This is here only to apply a max width to the contents ?>
 	<div class="inner">
+
+		<?// Back button is first so floating works correctly ?>
+		<? if (!empty($back) && !Str::is('decoy\account*', Route::currentRouteName())): ?>
+			<a href="<?=$back?>" class="back">
+				<span class="glyphicon glyphicon-arrow-left"></span>
+				Back to listing
+			</a>
+		<? endif?>
+
 		<?// The breadcrumbs ?>
 		<a href="/admin"><span class="glyphicon glyphicon-home"></span></a>
 		<? foreach($breadcrumbs as $url => $name): ?>
@@ -16,13 +25,6 @@ if (empty($breadcrumbs)) return;
 				<span class="glyphicon glyphicon-chevron-right"></span>
 			<? endif ?>
 		<? endforeach ?>
-		
-		<?// Back button ?>
-		<? if (!empty($back) && !Str::is('decoy\account*', Route::currentRouteName())): ?>
-			<a href="<?=$back?>" class="back">
-				<span class="glyphicon glyphicon-arrow-left"></span>
-				Back to listing
-			</a>
-		<? endif?>
+
 	</div>
 </div>
