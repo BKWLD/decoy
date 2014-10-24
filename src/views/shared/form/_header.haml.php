c6fe3423b3d56e546f56eff3e04897f254d95604
Robert Reinhard <robert.reinhard@bkwld.com>
2014-10-24 16:49:16 -0700

    Making sidebar not required

--- src/views/shared/form/_header.haml.php
+++ src/views/shared/form/_header.haml.php
@@ -1,11 +1,11 @@
 -# Show validation errors
 !=View::make('decoy::shared.form._errors')
 
--if($sidebar->isEmpty())
-	-# Open a horizontal, non-sidebar form
-	!=Former::horizontal_open_for_files()
-
--else
+-if(isset($sidebar) && !$sidebar->isEmpty())
 	- # Open a related, vertical form, including it's columns
 	!='<div class="row"><div class="col-md-7 related-left-col">'
 	!=Former::vertical_open_for_files()
+	
+-else
+	-# Open a horizontal, non-sidebar form
+	!=Former::horizontal_open_for_files()
