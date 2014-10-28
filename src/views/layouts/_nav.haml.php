4d40ba5e1fe38c8e3668db248180f2f9a7c430b7
Robert Reinhard <robert.reinhard@bkwld.com>
2014-10-28 14:18:53 -0700

    Renaming "nav-arrow" to "parent" to describe what it is and tweaking :active

--- src/views/layouts/_nav.haml.php
+++ src/views/layouts/_nav.haml.php
@@ -18,7 +18,7 @@
 			
 				-if (!empty($page->children))
 					.main-nav(class=$page->active?'active open':null)
-						%a.top-level.nav-arrow
+						%a.top-level.parent
 							-if($page->icon)
 								%span.glyphicon(class="glyphicon-#{$page->icon}")
 							!=$page->label
@@ -41,7 +41,7 @@
 
 			-if($auth->developer())
 				.main-nav(class=(in_array(Request::segment(2), ['admins', 'commands', 'workers']))?'active open':null)
-					%a.top-level.nav-arrow
+					%a.top-level.parent
 						%span.glyphicon.glyphicon-cog
 						Admin
 
