a8329edf99bf3755b4537c10f2faae069efffd19
Robert Reinhard <robert.reinhard@bkwld.com>
2014-10-28 09:52:39 -0700

    Fixing login error display

--- src/views/account/_error.php
+++ src/views/account/_error.php
@@ -1,13 +1,13 @@
 <?// Show an alert box with data passed via the session. ?>
 <? if (Session::has('login_error')): ?>
-	<div class="alert alert-error">
+	<div class="alert alert-danger" role="alert">
 		<button type="button" class="close" data-dismiss="alert">×</button>
 		<?= Session::get('login_error') ?>
 	</div>
 <? endif ?>
 
 <? if (Session::has('login_notice')): ?>
-	<div class="alert alert-notice">
+	<div class="alert alert-warning" role="alert">
 		<button type="button" class="close" data-dismiss="alert">×</button>
 		<?= Session::get('login_notice') ?>
 	</div>
