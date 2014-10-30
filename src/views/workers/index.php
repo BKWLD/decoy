27e97643aa0cf3a038f229f890bc06d14c9ee74b
Robert Reinhard <robert.reinhard@bkwld.com>
2014-10-30 12:32:45 -0700

    Fixing styling on workers and commands

--- src/views/workers/index.php
+++ src/views/workers/index.php
@@ -1,29 +1,32 @@
+<? foreach($workers as $worker): ?>
+	<fieldset data-js-view="worker" data-log-url=<?=route('decoy\workers@tail', strtolower(urlencode($worker->getName())))?> data-interval="<?=$worker->currentInterval('raw')?>">
+		<div class="legend sidebar-header"><?=ucwords(str_replace(':', ' : ', $worker->getName())) ?>
 
-	<ul id="workers" class="list-unstyled">
-		
-		<? foreach($workers as $worker): ?>
-			<div class="standard-list" data-js-view="worker" data-log-url=<?=route('decoy\workers@tail', strtolower(urlencode($worker->getName())))?> data-interval="<?=$worker->currentInterval('raw')?>">
-				<div class="legend sidebar-header"><?=ucwords(str_replace(':', ' : ', $worker->getName())) ?>
-
-					<div class="pull-right actions">
-						<span class="status <?=$worker->isRunning()?'ok':'fail'?>">Rate: <strong><?=$worker->currentInterval('abbreviated')?></strong></span>
-						<a class="btn btn-default">Logs</a>
-					</div>
-
-				</div>
-				<li class="worker-entry">
-					
-					<?=HTML::tag($worker->getDescription())?>
-					
-					<ul>
-						<li>Last worker execution: <?=$worker->lastHeartbeat()?></li>
-						<li>Last heartbeat<?if(!$worker->isRunning()):?> (and execution)<?endif?>: <?=$worker->lastHeartbeatCheck()?></li>
-						<li>Currently executing every: <?=$worker->currentInterval()?></li>
-					</ul>
-					
-					<div class="log closed">Loading...</div>
-				</li>
+			<div class="pull-right actions">
+				<span class="status">
+					<? if ($worker->isRunning() == 'ok'): ?>
+						<span class="glyphicon glyphicon-ok"></span>
+					<? else: ?>
+						<span class="glyphicon glyphicon-question-sign"></span>
+					<? endif ?>
+					Rate: <strong><?=$worker->currentInterval('abbreviated')?></strong>
+					</span>
+				<a class="btn btn-sm outline">Logs</a>
 			</div>
-		<? endforeach ?>
 
-	</ul>
+		</div>
+		<div class="worker-entry">
+			
+			<?=HTML::tag($worker->getDescription())?>
+			
+			<ul>
+				<li>Last worker execution: <?=$worker->lastHeartbeat()?></li>
+				<li>Last heartbeat<?if(!$worker->isRunning()):?> (and execution)<?endif?>: <?=$worker->lastHeartbeatCheck()?></li>
+				<li>Currently executing every: <?=$worker->currentInterval()?></li>
+			</ul>
+			
+			<div class="log closed">Loading...</div>
+		</div>
+	</fieldset>
+<? endforeach ?>
+
