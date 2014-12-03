eb9518bbde8ba6d0ca5681fa59fd4074c1de132d
Robert Reinhard <robert.reinhard@bkwld.com>
2014-12-03 10:39:06 -0800

    Making the tooltip one word on search to fix tooltip bug causing bad arrow placement
    Fixes #378

--- src/views/shared/list/_full_header.haml.php
+++ src/views/shared/list/_full_header.haml.php
@@ -17,8 +17,8 @@
 				%a.btn.btn-sm.outline.search-toggle
 					.glyphicon.glyphicon-search
 
-				-# Rest button, change the default container to fix a Chrome issue https://github.com/BKWLD/decoy/issues/239
-				%a.btn.btn-sm.outline.search-clear.js-tooltip(data-container=".full-header .btn-toolbar" title="Reset search")
+				-# Rest button
+				%a.btn.btn-sm.outline.search-clear.js-tooltip(title="Reset")
 					.glyphicon.glyphicon-ban-circle
 
 -# Search UI
