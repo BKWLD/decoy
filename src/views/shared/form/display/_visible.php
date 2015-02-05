4fb552e4a07a9c8e6cb0cd216f621ff30da1942b
Robert Reinhard <robert.reinhard@bkwld.com>
2015-02-05 11:08:35 -0800

    Fixing missing space in visiblility module

--- src/views/shared/form/display/_visible.php
+++ src/views/shared/form/display/_visible.php
@@ -4,5 +4,5 @@
 	'Visible' => array('value' => '1', 'checked' => empty($item) ? true : $item->visible == 1),
 	'Hidden' => array('value' => '', 'checked' => empty($item) ? false : $item->visible != 1),
 ))->blockHelp((!empty($item) && $uri = $item->getUriAttribute()) ? 
-	'If hidden, this content will be accessible via it\'s<a href="'.$uri.'">URI</a> but not in lists' : 
+	'If hidden, this content will be accessible via it\'s <a href="'.$uri.'">URI</a> but not in lists' : 
 	'If hidden, this content will be completely inaccessible') ?>
