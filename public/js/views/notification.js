932b21befb87522fc753f9faf4386a2f7b30b185
Robert Reinhard <robert.reinhard@bkwld.com>
2015-02-12 14:26:00 -0800

    Removing BRs
    RE: previous commit

--- public/js/views/notification.js
+++ public/js/views/notification.js
@@ -108,7 +108,7 @@ define(function(require) {
 		var message = "<strong>Server request failed</strong>";
 		if( response !== null && typeof response != 'undefined') {
 			if( response.responseJSON !== null && typeof response.responseJSON != 'undefined')
-				message += '<br/><br/><pre>' + JSON.stringify(response.responseJSON) + '</pre>';
+				message += '<pre>' + JSON.stringify(response.responseJSON) + '</pre>';
 		}
 		this.set( message, 'danger', true );
 	};
