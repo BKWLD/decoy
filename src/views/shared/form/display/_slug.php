4b7b3f30fa428240754b901e59e07545a2141379
Robert Reinhard <robert.reinhard@bkwld.com>
2015-03-12 21:00:41 -0700

    Supprt periods in the URL path

--- src/views/shared/form/display/_slug.php
+++ src/views/shared/form/display/_slug.php
@@ -6,7 +6,7 @@ if (!empty($item->slug)) {
 	$prepend = '/';
 	if ($url = $item->getUriAttribute()) {
 		$url_link = '<a href="'.$url.'">URI</a>';
-		$prepend = preg_replace('#/[\w-]+$#', '/', parse_url(rtrim($url,'/'), PHP_URL_PATH));
+		$prepend = preg_replace('#/[\w-\.]+$#', '/', parse_url(rtrim($url,'/'), PHP_URL_PATH));
 	}
 	echo Former::text('slug')->blockHelp('Used to form the '.$url_link.' for this content.')->prepend($prepend);
 }
