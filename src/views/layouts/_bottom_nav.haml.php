354c0ae34975cff306bdd719e48b389aa8dd768d
Robert Reinhard <robert.reinhard@bkwld.com>
2015-03-31 11:33:14 -0700

    Incrementing displayed version

--- src/views/layouts/_bottom_nav.haml.php
+++ src/views/layouts/_bottom_nav.haml.php
@@ -10,7 +10,7 @@
 				Logout
 			.subtitle
 				%span.glyphicon.glyphicon-heart
-				Decoy 4.3 by
+				Decoy 4.5 by
 				%a(href="http://bkwld.com" target="_blank") BKWLD
 		%a.right(href="/")
 			.glyphicon.glyphicon-globe
