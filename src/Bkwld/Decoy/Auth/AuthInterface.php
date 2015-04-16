b07ec5c8ab6894998bb74356b68ac3d5ffb1fb51
Robert Reinhard <robert.reinhard@bkwld.com>
2015-04-16 09:35:43 -0700

    Better comments on interface

--- src/Bkwld/Decoy/Auth/AuthInterface.php
+++ src/Bkwld/Decoy/Auth/AuthInterface.php
@@ -5,18 +5,84 @@
  */
 interface AuthInterface {
 	
-	// Methods for inspecting properties of the user
-	public function check();            // Boolean for whether the user is logged in and an admin
-	public function role($role = null); // Check if a user is in a specific role or return the list of all roles
-	public function developer();        // Boolean as to whether the user has developer entitlements
-	
-	// Urls related to the login process
-	public function loginAction();      // URL that contains the login page
-	public function logoutUrl();        // URL to process logging out
-	public function deniedUrl();        // URL to redirec to if a user doesn't have permission for the page
+	/**
+	 * ---------------------------------------------------------------------------
+	 * Methods for inspecting properties of the user
+	 * ---------------------------------------------------------------------------
+	 */
 
-	// These return properites of the logged in user
-	public function userPhoto();        // Avatar photo for the header
-	public function userName();         // Name to display in the header for the user
-	public function userUrl();          // URL to the user's profile page in the admin	
+	/**
+	 * Boolean for whether the user is logged in and an admin
+	 * @return boolean
+	 */
+	public function check();
+
+	/**
+	 * Check if a user is in a specific role or return the list of all roles
+	 * 
+	 * @param  string $role
+	 * @return boolean | array
+	 */
+	public function role($role = null);
+	
+	/**
+	 * Boolean as to whether the user has developer entitlements
+	 * 
+	 * @return boolean
+	 */
+	public function developer();
+	
+	/**
+	 * ---------------------------------------------------------------------------
+	 * Methods for inspecting properties of the user
+	 * ---------------------------------------------------------------------------
+	 */
+
+	/**
+	 * URL that contains the login page
+	 * 
+	 * @return string
+	 */
+	public function loginAction();
+	
+	/**
+	 * URL to process logging out
+	 * 
+	 * @return string
+	 */
+	public function logoutUrl();
+	
+	/**
+	 * URL to redirect to if a user doesn't have permission for the page
+	 * 
+	 * @return string
+	 */
+	public function deniedUrl();
+
+	/**
+	 * ---------------------------------------------------------------------------
+	 * These return properites of the logged in user
+	 * ---------------------------------------------------------------------------
+	 */
+
+	/**
+	 * Avatar photo for the header
+	 * 
+	 * @return string
+	 */
+	public function userPhoto();
+
+	/**
+	 * Name to display in the header for the user
+	 * 
+	 * @return string
+	 */
+	public function userName();
+
+	/**
+	 * URL to the user's profile page in the admin
+	 * 
+	 * @return string
+	 */
+	public function userUrl();
 }
