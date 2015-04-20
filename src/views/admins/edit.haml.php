f082c3b20e6f8f246e6d512eed983753048c8876
Robert Reinhard <robert.reinhard@bkwld.com>
2015-04-20 11:06:31 -0700

    Hiding "manage" fields from admin editor if you're not a manager

--- src/views/admins/edit.haml.php
+++ src/views/admins/edit.haml.php
@@ -5,7 +5,6 @@
 
 	!= Former::text('first_name') 
 	!= Former::text('last_name') 
-	!= Former::image('image') 
 
 	!= Former::text('email')
 	-if (Config::get('decoy::core.obscure_admin_password'))
@@ -14,14 +13,16 @@
 	-else
 		!= Former::text('password')->forceValue(empty($item)?Str::random(16):null)->placeholder(empty($item)?null:'Leave blank to prevent change')
 
-	-if (($roles = Config::get('decoy::site.roles')) && !empty($roles))
+	!= Former::image('image') 
+
+	-if (app('decoy.auth')->can('manage', $controller) && ($roles = Config::get('decoy::site.roles')) && !empty($roles))
 		!= Former::radios('role')->radios(Bkwld\Library\Laravel\Former::radioArray($roles))
 
 	!= Former::checkbox('_send_email', ' ')->value(1)->text(empty($item)?'Send welcome email, including password':'Email '.$item->first_name.' with login changes')
 
 	-# Create moderation actions
 	-ob_start()
-	-if (!empty($item) && app('decoy.auth')->can('update', $controller))
+	-if (!empty($item) && app('decoy.auth')->can('manage', $controller))
 
 		-# Disable admin
 		-if (!$item->disabled())
