21503b316d471e0c60369b774b37748e669331cc
Robert Reinhard <robert.reinhard@bkwld.com>
2015-06-09 16:37:50 -0700

    Setting the field correctly

--- public/js/views/search.js
+++ public/js/views/search.js
@@ -238,7 +238,8 @@ define(function (require) {
 				var $condition = this.add();
 
 				// Restore choices
-				$condition.find('.comparisons').val(condition[0]);
+				
+				$condition.find('.fields').val(condition[0]);
 				this.change(); // Have to trigger handler manually
 				$condition.find('.comparisons').val(condition[1]);
 				$condition.find('.input-field').val(condition[2]);
