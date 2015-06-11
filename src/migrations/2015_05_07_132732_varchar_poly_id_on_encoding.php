a9bb1f75329993118840f992938e9de114c7f5b9
Robert Reinhard <robert.reinhard@bkwld.com>
2015-06-11 14:50:49 -0700

    Making alters SQL Server safe

--- src/migrations/2015_05_07_132732_varchar_poly_id_on_encoding.php
+++ src/migrations/2015_05_07_132732_varchar_poly_id_on_encoding.php
@@ -12,7 +12,29 @@ class VarcharPolyIdOnEncoding extends Migration {
 	 */
 	public function up()
 	{
-		DB::statement("ALTER TABLE `encodings` CHANGE `encodable_id` `encodable_id` VARCHAR(255)  NOT NULL");
+
+		// Need to remove indexes for SqlServer to migrate
+		Schema::table('encodings', function($table) {
+			$table->dropIndex('encodings_encodable_id_encodable_type_encodable_attribute_index');
+			$table->dropIndex('encodings_encodable_type_encodable_id_encodable_attribute_index');
+			$table->dropIndex('encodings_encodable_attribute_encodable_id_encodable_type_index');
+		});
+
+		// SQL SERVER
+		if (Config::get('database.default') == 'sqlsrv') {
+			DB::statement("ALTER table encodings ALTER COLUMN encodable_id VARCHAR(255)");
+
+		// MySQL	
+		} else {
+			DB::statement("ALTER TABLE `encodings` CHANGE `encodable_id` `encodable_id` VARCHAR(255)  NOT NULL");
+		}
+
+		Schema::table('encodings', function($table) {
+			$table->index(array('encodable_id', 'encodable_type', 'encodable_attribute'));
+			$table->index(array('encodable_type', 'encodable_id', 'encodable_attribute'));
+			$table->index(array('encodable_attribute', 'encodable_id', 'encodable_type'));
+		});
+		
 	}
 
 	/**
@@ -22,7 +44,14 @@ class VarcharPolyIdOnEncoding extends Migration {
 	 */
 	public function down()
 	{
-		DB::statement("ALTER TABLE `encodings` CHANGE `encodable_id` `encodable_id` INT(10)  NOT NULL");
+		// SQL SERVER
+		if (Config::get('database.default') == 'sqlsrv') {
+			
+
+		// MySQL	
+		} else {
+			DB::statement("ALTER TABLE `encodings` CHANGE `encodable_id` `encodable_id` INT(10)  NOT NULL");
+		}
 	}
 
 }
