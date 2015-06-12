b126a8a07d5e0a831d85d954f30af5e82c7e505a
Robert Reinhard <robert.reinhard@bkwld.com>
2015-06-12 08:29:46 -0700

    Making display module HAML so it can be copy-pasted easier

---
+++ src/views/shared/form/_display_module.haml.php
@@ -0,0 +1,4 @@
+!= View::make('decoy::shared.form.display._legend', $__data)
+!= View::make('decoy::shared.form.display._slug', $__data)
+!= View::make('decoy::shared.form.display._visible', $__data)
+!= View::make('decoy::shared.form.display._locale', $__data)
