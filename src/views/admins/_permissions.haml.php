d58c4325662803d555cbd165ad9bac08cfe8d966
Robert Reinhard <robert.reinhard@bkwld.com>
2015-06-30 15:34:07 -0700

    Skip over controllers with no permissions

--- src/views/admins/_permissions.haml.php
+++ src/views/admins/_permissions.haml.php
@@ -12,12 +12,14 @@
 
 		.permissions-list
 			-foreach(Admin::getPermissionOptions($item) as $controller)
+				-if(!($permissions = $controller->permissions) || empty($permissions)) continue
+
 				.controller
 					%span.title.js-tooltip(title=$controller->description) = $controller->title
 
 					.controller-permissions
 						%input(type='hidden' name="_permission[#{$controller->slug}][]")
-						-foreach($controller->permissions as $permission)
+						-foreach($permissions as $permission)
 							%label.controller-permission
 								%input(type='checkbox' name="_permission[#{$controller->slug}][]" value=$permission->slug checked=$permission->checked)
 								%span.js-tooltip(title=$permission->description) = $permission->title
