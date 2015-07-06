15e486c841f98f9d802ebc65c0d2dea494395082
Robert Reinhard <robert.reinhard@bkwld.com>
2015-07-06 12:29:53 -0700

    Check the initial permissions boxes

--- src/Bkwld/Decoy/Models/Admin.php
+++ src/Bkwld/Decoy/Models/Admin.php
@@ -264,16 +264,21 @@ class Admin extends Base implements UserInterface, RemindableInterface {
 
 				// Add permission options for the controller 
 				'permissions' => array_map(function($value, $key) use ($class, $admin) {
+					$roles = array_keys(Config::get('decoy::site.roles'));
 					return (object) [
 						'slug' => $key,
 						'title' => is_array($value) ? $value[0] : String::titleFromKey($key),
 						'description' => is_array($value) ? $value[1] : $value,
-						'checked' => false,
+
+						// Set the initial checked state based on the admin's permissions, if
+						// one is set.  Or based on the first role.
+						'checked' => $admin ? 
+							app('decoy.auth')->can($key, $class, $admin) :
+							app('decoy.auth')->can($key, $class, $roles[0]),
 
 						// Filter the list of roles to just the roles that allow the
 						// permission currently being iterated through
-						'roles' => array_filter(array_keys(Config::get('decoy::site.roles')), 
-							function($role) use ($key, $class) {
+						'roles' => array_filter($roles, function($role) use ($key, $class) {
 							return app('decoy.auth')->can($key, $class, $role);
 						}),
 
