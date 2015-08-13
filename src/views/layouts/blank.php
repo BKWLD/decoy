f9605cdb382335a845c72278c24fbbba7db7b9f5
Robert Reinhard <robert.reinhard@bkwld.com>
2015-08-13 09:06:03 -0700

    Copying more layout stuff over from default layout

--- src/views/layouts/blank.php
+++ src/views/layouts/blank.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>  <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>     <html class="no-js lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>     <html class="no-js lt-ie10 lt-ie9"> <![endif]-->
+<!--[if IE 9]>     <html class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
 	<head>
 		<meta charset="utf-8"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 		<?= Decoy::title() ?>
-		<meta name="viewport" content="width=device-width"/>
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
 		<meta name="csrf" content="<?=Session::getToken()?>"/>
 		<link rel="stylesheet" href="<?=HTML::grunt('/css/admin/vendor.css')?>"/>
 		<link rel="stylesheet" href="<?=HTML::grunt('/css/admin/style.css')?>"/>
