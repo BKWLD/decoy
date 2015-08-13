30fbd35bfed183e4ea77639897c1d4b97f6ee212
Robert Reinhard <robert.reinhard@bkwld.com>
2015-08-13 16:02:29 -0700

    Fix Former config

--- src/Bkwld/Decoy/ServiceProvider.php
+++ src/Bkwld/Decoy/ServiceProvider.php
@@ -82,16 +82,16 @@ class ServiceProvider extends BaseServiceProvider {
 		require_once(__DIR__.'/../../composers/shared.list._search.php');
 
 		// Use Bootstrap 3
-		Config::set('former::framework', 'TwitterBootstrap3');
+		Config::set('former.framework', 'TwitterBootstrap3');
 
 		// Reduce the horizontal form's label width
-		Config::set('former::TwitterBootstrap3.labelWidths', []);
+		Config::set('former.TwitterBootstrap3.labelWidths', []);
 
 		// Change Former's required field HTML
-		Config::set('former::required_text', ' <span class="glyphicon glyphicon-exclamation-sign js-tooltip required" title="Required field"></span>');
+		Config::set('former.required_text', ' <span class="glyphicon glyphicon-exclamation-sign js-tooltip required" title="Required field"></span>');
 
 		// Make pushed checkboxes have an empty string as their value
-		Config::set('former::unchecked_value', '');
+		Config::set('former.unchecked_value', '');
 
 		// Add Decoy's custom Fields to Former so they can be invoked using the "Former::"
 		// namespace and so we can take advantage of sublassing Former's Field class.
