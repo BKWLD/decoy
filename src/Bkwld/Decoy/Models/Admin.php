2c714eddd9ed3b6605152d1fcf766d49882075e8
Robert Reinhard <robert.reinhard@bkwld.com>
2015-08-13 16:46:16 -0700

    Unnecessary dep

--- src/Bkwld/Decoy/Models/Admin.php
+++ src/Bkwld/Decoy/Models/Admin.php
@@ -8,7 +8,6 @@ use Decoy;
 use DecoyURL;
 use HTML;
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
