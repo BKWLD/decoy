f8c0afea752c7802628b132c2276b9b10ef16e86
Robert Reinhard <robert.reinhard@bkwld.com>
2015-08-14 15:04:12 -0700

    Porting Auth filter
    #496 #489

--- src/Bkwld/Decoy/Middleware/Auth.php
+++ src/Bkwld/Decoy/Middleware/Auth.php
@@ -1,10 +1,15 @@
 <?php namespace Bkwld\Decoy\Middleware;
 
 // Deps
+use App;
+use Config;
 use Closure;
+use Route;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
- * Endorce access restrictions 
+ * Endorce admin login and also test their permission to access the current
+ * request
  */
 class Auth {
 
@@ -17,9 +22,83 @@ class Auth {
 	*/
 	public function handle($request, Closure $next) {
 
-		// dd('In here');
+		// Require a logged in user
+		if (!App::make('decoy.auth')->check()) return App::make('decoy.acl_fail');
 
+		// Determine the action and controller differently depending on how the
+		// request is routed.
+		if (Route::is('decoy::wildcard')) {
+			list($action, $controller) = $this->dectectFromWildcardRouter();
+		} else {
+			list($action, $controller) = $this->dectectFromExplicitRoute();
+		}
+
+		// If they don't hvae permission, throw an error
+		if (!App::make('decoy.auth')->can($action, $controller)) {
+			throw new AccessDeniedHttpException;
+		}
+
+		// All is well, continue
 		return $next($request);
+	}
+
+	/**
+	 * Get the actino and controller from an explicilty defined route
+	 *
+	 * @return array action,controller
+	 */
+	protected function dectectFromExplicitRoute() {
+
+		// Get parse the `uses` from the route definition
+		preg_match('#(.+)@(.+)#', Route::current()->getActionName(), $matches);
+		$controller = $matches[1];
+		$action = $matches[2];
+
+		// Further mapping of the action
+		$action = $this->mapActionToPermission($action);
+
+		// Return the detected action and controller
+		return [$action, $controller];
+	}
+
+	/**
+	 * Get the action and controller from the wildcard router
+	 *
+	 * @return array action,controller
+	 */
+	protected function dectectFromWildcardRouter() {
+		$wildcard = App::make('decoy.wildcard');
+
+		// Attach / detach are ACL-ed by the parent controller.  It's the one being touched		
+		$action = $wildcard->detectAction();
+		if (in_array($action, ['attach', 'remove'])) {
+			$controller = Input::get('parent_controller');
+			$action = 'update';
+
+		// Otherwise, use the controller from the route
+		} else $controller = $wildcard->detectControllerName();
+
+		// Further mapping of the action
+		$action = $this->mapActionToPermission($action);
+
+		// Return the detected action and controller
+		return [$action, $controller];
+	}
+
+	/**
+	 * Map the actions from the wildcard router into the smaller set supported by
+	 * the Decoy permissions system
+	 */
+	protected function mapActionToPermission($action) {
+		switch($action) {
+			case 'new':
+			case 'store': return 'create';
+			case 'edit':
+			case 'autocomplete':
+			case 'index':
+			case 'indexChild': return 'read';
+			default: return $action;
+		}
 	}
 
 }
