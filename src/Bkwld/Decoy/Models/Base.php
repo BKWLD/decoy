bdd99091e84575b5f5c0672e2f7c2b5e8e4c0114
Robert Reinhard <robert.reinhard@bkwld.com>
2016-01-04 13:25:48 -0800

    Whitespace

--- src/Bkwld/Decoy/Models/Base.php
+++ src/Bkwld/Decoy/Models/Base.php
@@ -33,13 +33,13 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	use SupportsUploads, Cloneable, SluggableTrait {
 		needsSlugging as traitNeedsSlugging;
 	}
-	
+
 	//---------------------------------------------------------------------------
 	// Overrideable properties
 	//---------------------------------------------------------------------------
-	
+
 	/**
-	 * This should be overridden by Models to store the array of their 
+	 * This should be overridden by Models to store the array of their
 	 * Laravel validation rules
 	 *
 	 * @var array
@@ -47,9 +47,9 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	static public $rules = [];
 
 	/**
-	 * This is should be overriden like so to specify crops that the image 
+	 * This is should be overriden like so to specify crops that the image
 	 * cropping widget should make UI.  Example:
-	 * 
+	 *
 	 * array('image' => array('marquee' => '4:3', 'feature'))
 	 *
 	 * @var array
@@ -57,7 +57,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	static public $crops = [];
 
 	/**
-	 * Should this model be localizable in the admin.  If not undefined, will 
+	 * Should this model be localizable in the admin.  If not undefined, will
 	 * override the site config "auto_localize_root_models"
 	 *
 	 * @var boolean
@@ -67,21 +67,21 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	/**
 	 * If false, this model cannot be cloned
 	 *
-	 * @var boolean 
+	 * @var boolean
 	 */
 	public $cloneable = true;
 
 	/**
 	 * Specify columns that shouldn't be duplicated by Bkwld\Cloner.  Include
 	 * slug by default so that Sluggable will automatically generate a new one.
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $clone_exempt_attributes = ['slug'];
 
 	/**
 	 * Relations to follow when models are duplicated
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $cloneable_relations;
@@ -89,16 +89,16 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	/**
 	 * If populated, these will be used instead of the files that are found
 	 * automatically by getCloneableFileAttributes()
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $cloneable_file_attributes;
-	
+
 	/**
 	 * Constructor registers events and configures mass assignment
 	 */
 	public function __construct(array $attributes = array()) {
-		
+
 		// Remove any settings the affect JSON conversion (visible / hidden) and
 		// mass assignment protection (fillable / guarded) while in the admin
 		if (Decoy::handling()) {
@@ -117,10 +117,10 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	}
 
 	// Disable all mutatators while in Admin by returning that no mutators exist
-	public function hasGetMutator($key) { 
+	public function hasGetMutator($key) {
 		return Decoy::handling() && array_key_exists($key, $this->attributes) ? false : parent::hasGetMutator($key);
 	}
-	public function hasSetMutator($key) { 
+	public function hasSetMutator($key) {
 		return Decoy::handling() && array_key_exists($key, $this->attributes) ? false : parent::hasSetMutator($key);
 	}
 
@@ -128,7 +128,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	 * No-Op callbacks invoked by Observers\ModelCallbacks.  These allow quick handling
 	 * of model event states.
 	 *
-	 * @return void|false 
+	 * @return void|false
 	 */
 	public function onSaving() {}
 	public function onSaved() {}
@@ -150,10 +150,10 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	 *
 	 * @return Illuminate\Database\Eloquent\Relations\Relation
 	 */
-	public function changes() { 
-		return $this->morphMany('Bkwld\Decoy\Models\Change', 'loggable', 'model', 'key'); 
+	public function changes() {
+		return $this->morphMany('Bkwld\Decoy\Models\Change', 'loggable', 'model', 'key');
 	}
-	
+
 	//---------------------------------------------------------------------------
 	// Slug creation via cviebrock/eloquent-sluggable
 	//---------------------------------------------------------------------------
@@ -161,7 +161,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	/**
 	 * Tell sluggable where to get the source for the slug and apply other
 	 * customizations.
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $sluggable = array(
@@ -171,8 +171,8 @@ abstract class Base extends Eloquent implements SluggableInterface {
 
 	/**
 	 * Check for a validation rule for a slug column
-	 * 
-	 * @return boolean 
+	 *
+	 * @return boolean
 	 */
 	protected function needsSlugging() {
 		if (!array_key_exists('slug', static::$rules)) return false;
@@ -184,11 +184,11 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	//---------------------------------------------------------------------------
 
 	/**
-	 * Return the title for the row for the purpose of displaying in admin list 
-	 * views and breadcrumbs.  It looks for columns that are named like common 
+	 * Return the title for the row for the purpose of displaying in admin list
+	 * views and breadcrumbs.  It looks for columns that are named like common
 	 * things that would be titles.
 	 *
-	 * @return string 
+	 * @return string
 	 */
 	public function getAdminTitleHtmlAttribute() {
 		return $this->croppaTag(40, 40).$this->getAdminTitleAttribute();
@@ -208,7 +208,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	/**
 	 * A no-op that should return the URI (an absolute path or a fulL URL) to the record
 	 *
-	 * @return string 
+	 * @return string
 	 */
 	public function getUriAttribute() { }
 
@@ -226,7 +226,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 
 		// Get all the model attributes from upchuck
 		if (method_exists($this, 'getUploadMap')) {
-			$attributes = array_unique(array_merge($attributes, 
+			$attributes = array_unique(array_merge($attributes,
 				array_values($this->getUploadMap())));
 		}
 
@@ -237,7 +237,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	/**
 	 * Use getFileAttributesAttribute() to get the files that should be cloned
 	 * by Bkwld\Cloner
-	 * 
+	 *
 	 * @return array The keys of all the attributes that store file references
 	 */
 	public function getCloneableFileAttributes() {
@@ -250,7 +250,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	/**
 	 * A no-op that can add classes to rows in listing tables in the admin
 	 *
-	 * @return string 
+	 * @return string
 	 */
 	public function getAdminRowClassAttribute() { }
 
@@ -264,7 +264,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	 * view
 	 *
 	 * @param array $data The data passed to a listing view
-	 * @return array 
+	 * @return array
 	 */
 	public function makeAdminActions($data) {
 		$actions = [];
@@ -283,17 +283,17 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	 */
 	protected function makeVisibilityAction($data) {
 		extract($data);
-		
+
 		// Check if this model supports editing the visibility
-		if ($many_to_many 
-			|| !app('decoy.auth')->can('publish', $controller) 
+		if ($many_to_many
+			|| !app('decoy.auth')->can('publish', $controller)
 			|| !array_key_exists('visible', $this->attributes)) return;
 
 		// Create the markup
 		$is_visible = $this->getAttribute('visible');
 		return sprintf('<a class="visibility js-tooltip" data-placement="left" title="%s">
 				<span class="glyphicon glyphicon-eye-%s"></span>
-			</a>', 
+			</a>',
 			$is_visible ? 'Make draft' : 'Publish',
 			$is_visible ? 'open' : 'close'
 		);
@@ -304,11 +304,11 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	 * Make the edit action.
 	 *
 	 * @param array $data The data passed to a listing view
-	 * @return string 
+	 * @return string
 	 */
 	protected function makeEditAction($data) {
 		extract($data);
-		return sprintf('<a href="%s" class="action-edit js-tooltip" 
+		return sprintf('<a href="%s" class="action-edit js-tooltip"
 			data-placement="left" title="Edit in admin">
 				<span class="glyphicon glyphicon-pencil"></span>
 			</a>', $this->getAdminEditUri($controller, $many_to_many));
@@ -316,11 +316,11 @@ abstract class Base extends Eloquent implements SluggableInterface {
 
 	/**
 	 * Get the admin edit URL assuming you know the controller and whether it's
-	 * being listed as a many to many 
+	 * being listed as a many to many
 	 *
 	 * @param string $controller ex: Admin\ArticlesController
 	 * @param boolean $many_to_many
-	 * @return string 
+	 * @return string
 	 */
 	public function getAdminEditUri($controller, $many_to_many = false) {
 		if ($many_to_many) return URL::to(DecoyURL::action($controller.'@edit', $this->getKey()));
@@ -331,11 +331,11 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	 * Make the view action
 	 *
 	 * @param array $data The data passed to a listing view
-	 * @return string 
+	 * @return string
 	 */
 	protected function makeViewAction($data) {
 		if (!$uri = $this->getUriAttribute()) return;
-		return sprintf('<a href="%s" target="_blank" class="action-view js-tooltip" 
+		return sprintf('<a href="%s" target="_blank" class="action-view js-tooltip"
 			data-placement="left" title="View on site">
 				<span class="glyphicon glyphicon-bookmark"></span>
 			</a>', $uri);
@@ -345,14 +345,14 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	 * Make the delete action
 	 *
 	 * @param array $data The data passed to a listing view
-	 * @return string 
+	 * @return string
 	 */
 	protected function makeDeleteAction($data) {
 		extract($data);
 
 		// Check if this model can be deleted.  This mirrors code found in the table
 		//  partial for generating the edit link on the title
-		if (!(app('decoy.auth')->can('destroy', $controller) 
+		if (!(app('decoy.auth')->can('destroy', $controller)
 			|| ($many_to_many && app('decoy.auth')->can('update', $parent_controller)))) return;
 
 		// Return markup
@@ -364,17 +364,17 @@ abstract class Base extends Eloquent implements SluggableInterface {
 			$many_to_many ? 'remove' : 'trash'
 		);
 	}
-	
+
 	//---------------------------------------------------------------------------
 	// Scopes
 	//---------------------------------------------------------------------------
-	
+
 	/**
 	 * Search the title (where "title" is the admin definiton of the title) for
 	 * the terms.  This is designed for the Decoy autocomplete
 	 *
 	 * @param  Illuminate\Database\Query\Builder $query
-	 * @param  string $term 
+	 * @param  string $term
 	 * @throws Bkwld\Decoy\Exceptions\Exception
 	 * @return Illuminate\Database\Query\Builder
 	 */
@@ -389,7 +389,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 
 		// Concatenate all the attributes with spaces and look for the term.
 		$source = DB::raw('CONCAT('.implode('," ",',$attributes).')');
-		return $exact ? 
+		return $exact ?
 			$query->where($source, '=', $term) :
 			$query->where($source, 'LIKE', "%$term%");
 	}
@@ -403,7 +403,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	public function scopeOrdered($query) {
 		return $query->orderBy($this->getTable().'.created_at', 'desc');
 	}
-	
+
 	/**
 	 * Get visible items
 	 *
@@ -413,7 +413,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	public function scopeVisible($query) {
 		return $query->where($this->getTable().'.visible', '1');
 	}
-	
+
 	/**
 	 * Get all visible items by the default order
 	 *
@@ -446,7 +446,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 		return $query->orderBy($this->getTable().'.position', 'asc')
 			->orderBy($this->getTable().'.created_at', 'desc');
 	}
-	
+
 	/**
 	 * Randomize the results in the DB.  This shouldn't be used for large datasets
 	 * cause it's not very performant
@@ -487,7 +487,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	 * Find by the slug and fail if missing.  Invokes methods from the
 	 * Sluggable trait.
 	 *
-	 * @param string $string 
+	 * @param string $string
 	 * @return Illuminate\Database\Eloquent\Model
 	 * @throws Illuminate\Database\Eloquent\ModelNotFoundException
 	 */
@@ -495,19 +495,19 @@ abstract class Base extends Eloquent implements SluggableInterface {
 		if ($item = static::findBySlug($slug)) return $item;
 		throw (new ModelNotFoundException)->setModel(get_called_class());
 	}
-	
+
 	//---------------------------------------------------------------------------
 	// Utility methods
 	//---------------------------------------------------------------------------
 
 	/**
 	 * Deduce the source for the title of the model
-	 * 
-	 * @return array 
+	 *
+	 * @return array
 	 */
 	public function titleAttributes() {
 
-		// Convert to an array so I can test for the presence of values. As an 
+		// Convert to an array so I can test for the presence of values. As an
 		// object, it would throw exceptions
 		$row = $this->getAttributes();
 
@@ -523,20 +523,20 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	 * through a relationship OR it may be named pivot_id out of convention (something
 	 * currently done in Decoy_Base_Controller->get_index_child()).  This function
 	 * checks for either
-	 * 
-	 * @return integer 
+	 *
+	 * @return integer
 	 */
 	public function pivotId() {
 		if (!empty($this->pivot->id)) return $this->pivot->id;
 		else if (!empty($this->pivot_id)) return $this->pivot_id;
 		else return null;
 	}
-	
+
 	/**
 	 * Form a croppa URL, taking advantage of being able to set more columns null.  Also,
 	 * provides an easier way to inform the source crops
-	 * 
-	 * @param int $width 
+	 *
+	 * @param int $width
 	 * @param int $height
 	 * @param string $field Where to find the source image.  May be a method name, defined on the model or a simple
 	 *                      string of the column name in the database
@@ -545,7 +545,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	 * @return string A croppa URL
 	 */
 	public function croppa($width = null, $height = null, $field = 'image', $crop_style = null, $options = null) {
-		
+
 		// Get the image src path
 		if (method_exists($this, $field)) $src = call_user_func(array($this, $field));
 		else $src = $this->getOriginal($field); // Get un-mutated value
@@ -555,25 +555,25 @@ abstract class Base extends Eloquent implements SluggableInterface {
 		if ($crop_style && !array_key_exists($field, static::$crops)) {
 			throw new \Exception("A crop style was passed for $field but no crops are defined for that field.");
 		}
-		
+
 		// Check if the crop style is valid
 		if ($crop_style && !Collection::keyOrValueExists($crop_style, static::$crops[$field])) {
 			throw new \Exception("Crop style '$crop_style' is not defined for the field: $field");
 		}
-		
+
 		// Default crop style is 'default'
 		if (!$crop_style && !empty(static::$crops[$field]) && Collection::keyOrValueExists('default', static::$crops[$field])) {
 			$crop_style = 'default';
 		}
-		
+
 		// If there is a crop value, add it to the options
 		if ($crop_style) {
 			$crops = json_decode($this->{$field.'_crops'});
-			
+
 			// Check if a crop style was set in the admin for this crop style
 			if (!empty($crops->$crop_style)) {
 				if (!is_array($options)) $options = array();
-				
+
 				// Add the trim instructions to the croppa options
 				$options = array_merge($options, array(
 					'trim_perc' => array(
@@ -585,16 +585,16 @@ abstract class Base extends Eloquent implements SluggableInterface {
 				));
 			}
 		}
-		
+
 		// Return the Croppa URL
 		return Croppa::url($src, $width, $height, $options);
-		
+
 	}
 
 	/**
 	 * Return an image tag using croppa data
-	 * 
-	 * @param int $width 
+	 *
+	 * @param int $width
 	 * @param int $height
 	 * @param string $field Where to find the source image.  May be a method name, defined on the model or a simple
 	 *                      string of the column name in the database
@@ -610,7 +610,7 @@ abstract class Base extends Eloquent implements SluggableInterface {
 	/**
 	 * Return inline, background style elements using croppa data
 	 *
-	 * @param int $width 
+	 * @param int $width
 	 * @param int $height
 	 * @param string $crop_style A key from the $crops property of the model
 	 * @param string $field Where to find the source image.  May be a method name, defined on the model or a simple
@@ -622,10 +622,10 @@ abstract class Base extends Eloquent implements SluggableInterface {
 		if (!($url = $this->croppa($width, $height, $field, $crop_style, $options))) return;
 		return "background-image:url('{$url}')";
 	}
-	
+
 	/**
 	 * Get the admin controller class for this model.
-	 * 
+	 *
 	 * @return string ex: Admin\ArticlesController
 	 */
 	static public function adminControllerClass() {
@@ -643,14 +643,14 @@ abstract class Base extends Eloquent implements SluggableInterface {
 		// Check if class exists before returning
 		if (class_exists($controller)) return $controller;
 	}
-	
+
 	/**
 	 * Add a field to the blacklist
 	 *
-	 * @param string $field 
+	 * @param string $field
 	 */
 	public function blacklist($field) {
 		$this->guarded[] = $field;
 	}
-	
+
 }
