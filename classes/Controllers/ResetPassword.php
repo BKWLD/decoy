73f87932b579a40dea8663e7b190fe5102e46060
Robert Reinhard <robert.reinhard@bkwld.com>
2016-01-07 13:48:35 -0800

    Getting password resetting working

--- classes/Controllers/ResetPassword.php
+++ classes/Controllers/ResetPassword.php
@@ -3,26 +3,26 @@
 // Dependencies
 use Auth;
 use Bkwld\Decoy\Models\Admin;
+use Decoy;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 use Former;
-use URL;
-use View;
 
 /**
  * Handle logging in of users.  This is based on the AuthController.php and
  * PasswordController that Laravel's `php artisan make:auth` generates.
  */
-class Login extends Base {
-  use ResetsPasswords;
+class ResetPassword extends Base {
+	use ResetsPasswords;
 
-  /**
-   * Display the form to request a password reset link.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function showLinkRequestForm() {
+	/**
+	 * Display the form to request a password reset link.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function showLinkRequestForm() {
 
-    // Pass validation rules
+		// Pass validation rules
 		Former::withRules(array(
 			'email' => 'required|email',
 		));
@@ -30,27 +30,36 @@ class Login extends Base {
 		// Set the breadcrumbs
 		$this->breadcrumbs(array(
 			route('decoy::account@login') => 'Login',
-			URL::current() => 'Forgot Password',
+			url()->current() => 'Forgot Password',
 		));
 
 		// Show the page
 		$this->title = 'Forgot Password';
 		$this->description = 'You know the drill.';
 		return $this->populateView('decoy::account.forgot');
-  }
+	}
 
-  /**
-   * Display the password reset view for the given token.
-   *
-   * If no token is present, display the link request form.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  string|null  $token
-   * @return \Illuminate\Http\Response
-   */
-  public function showResetForm(Request $request, $token = null) {
+	/**
+	 * Get the e-mail subject line to be used for the reset link email.
+	 *
+	 * @return string
+	 */
+	protected function getEmailSubject() {
+		return 'Recover access to '.Decoy::site();
+	}
 
-    // Pass validation rules
+	/**
+	 * Display the password reset view for the given token.
+	 *
+	 * If no token is present, display the link request form.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  string|null  $token
+	 * @return \Illuminate\Http\Response
+	 */
+	public function showResetForm(Request $request, $token = null) {
+
+		// Pass validation rules
 		Former::withRules(array(
 			'email'                 => 'required|email',
 			'password'              => 'required',
@@ -59,14 +68,14 @@ class Login extends Base {
 
 		// Lookup the admin
 		$user = Admin::where('token', $token)
-			->join('password_reminders', 'password_reminders.email', '=', 'admins.email')
+			->join('password_resets', 'password_resets.email', '=', 'admins.email')
 			->firstOrFail();
 
 		// Set the breadcrumbs
 		$this->breadcrumbs(array(
 			route('decoy::account@login') => 'Login',
 			route('decoy::account@forgot') => 'Forgot Password',
-			URL::current() => 'Reset Password',
+			url()->current() => 'Reset Password',
 		));
 
 		// Show the page
@@ -74,17 +83,18 @@ class Login extends Base {
 		$this->description = 'Almost done.';
 		return $this->populateView('decoy::account.reset', [
 			'user' => $user,
+			'token' => $token,
 		]);
-  }
+	}
 
 	/**
-   * Get the post register / login redirect path. This is set to the login route
-   * so that the guest middleware can pick it up and redirect to the proper
-   * start page.
-   *
-   * @return string
-   */
-  public function redirectPath() {
-    return route('decoy::account@login');
-  }
+	 * Get the post register / login redirect path. This is set to the login route
+	 * so that the guest middleware can pick it up and redirect to the proper
+	 * start page.
+	 *
+	 * @return string
+	 */
+	public function redirectPath() {
+		return route('decoy::account@login');
+	}
 }
