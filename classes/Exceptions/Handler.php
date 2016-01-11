ad95523c2b0c09835568b45d81d416bc2a83de6e
Robert Reinhard <robert.reinhard@bkwld.com>
2016-01-11 15:36:23 -0800

    Whitespace

--- classes/Exceptions/Handler.php
+++ classes/Exceptions/Handler.php
@@ -23,7 +23,7 @@ class Handler extends AppHandler {
 	public function render($request, BaseException $e) {
 
 		// Check for custom handling
-		if ($response = $this->handle404s($e)) return $response;
+		if ($response = $this->handle404s($request, $e)) return $response;
 		if ($response = $this->handleCSRF($e)) return $response;
 		if ($response = $this->handleValidation($request, $e)) return $response;
 
@@ -32,18 +32,20 @@ class Handler extends AppHandler {
 	}
 
 	/**
-	 * If a 404 exception, check if there is a redirect rule
+	 * If a 404 exception, check if there is a redirect rule.  Or return a simple
+	 * header if an AJAX request.
 	 *
+	 * @param  \Illuminate\Http\Request  $request
 	 * @param  \Exception  $e
 	 * @return \Illuminate\Http\RedirectResponse
 	 */
-	protected function handle404s(BaseException $e) {
+	protected function handle404s($request, BaseException $e) {
 
 		// Check for right exception
-		if (!is_a($e, ModelNotFoundException::class) 
+		if (!is_a($e, ModelNotFoundException::class)
 			&& !is_a($e, NotFoundHttpException::class)) return;
 
-		// Check for 404
+		// Check for a valid redirect
 		if ($rule = RedirectRule::matchUsingRequest()->first()) {
 			return redirect($rule->to, $rule->code);
 		}
@@ -75,7 +77,7 @@ class Handler extends AppHandler {
 
 		// Log validation errors so Reporter will output them
 		// if (Config::get('app.debug')) Log::debug(print_r($e->validation->messages(), true));
-		
+
 		// Respond
 		if ($request->ajax()) {
 			return response()->json($e->validation->messages(), 400);
@@ -85,5 +87,4 @@ class Handler extends AppHandler {
 
 	}
 
-} 
-
+}
