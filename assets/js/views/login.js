4f8fe48e59725c33099edde060c0f0f5891ee37d
Robert Reinhard <robert.reinhard@bkwld.com>
2016-01-13 11:10:47 -0800

    Make the rgb2hex work with rgba() values

--- assets/js/views/login.js
+++ assets/js/views/login.js
@@ -58,7 +58,7 @@ define(function (require) {
 	// http://stackoverflow.com/a/3627747
 	function rgb2hex(rgb) {
 		if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
-		rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+		rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)/);
 		function hex(x) { return ("0" + parseInt(x).toString(16)).slice(-2); }
 		return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 	}
