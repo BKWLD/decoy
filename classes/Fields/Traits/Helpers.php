6b83d585a924baf2507cc8b7305f9990442a7ba8
Michael Starks <michael.starks@bkwld.com>
2016-01-21 15:51:44 -0800

    Fragments was deprecated

--- classes/Fields/Traits/Helpers.php
+++ classes/Fields/Traits/Helpers.php
@@ -38,13 +38,8 @@ trait Helpers {
 		// Use explicilty set model
 		if ($this->model) return $this->model;
 
-		// If a Fragment, build a model instance using the name of the field.  The input
-		// field uses pipes instead of the dots that are in the DB.
-		if (Route::is('decoy::fragments')) {
-			$model = Fragment::where('key', '=', str_replace('|', '.', $this->name))->first();
-
-		// Otherwise, just use the model that was passed to populator
-		} else $model = app('former.populator')->all();
+		// Get the model instance that was passed to Former by the Base Controller
+		$model = app('former.populator')->all();
 
 		// Make sure it's a model instance
 		if (is_a($model, 'Illuminate\Database\Eloquent\Model')) return $model;
