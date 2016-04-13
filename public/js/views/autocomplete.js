c5d79e69da0636c0e91175caad8e90d1af91df79
Robert Reinhard <robert.reinhard@bkwld.com>
2016-04-13 12:46:13 -0700

    Reverting to older version of typeahead to solve woes
    This is what 4.x used anyway

--- public/js/views/autocomplete.js
+++ public/js/views/autocomplete.js
@@ -3,36 +3,37 @@
 // extended by other views that need extended feature
 // --------------------------------------------------
 define(function (require) {
-	
+
 	// dependencies
 	var $ = require('jquery')
 		, _ = require('underscore')
 		, Backbone = require('backbone')
-		, Bloodhound = require('exports?Bloodhound!typeahead.js')
+		, Bloodhound = require('imports?define=>false&require=>false!exports?Bloodhound!typeahead.js')
 	;
+
 	// public view module
 	var Autocomplete = Backbone.View.extend({
-		
+
 		// Initial state and inheritable vars
 		found: false,
 		data: {}, // Stores the key (label) - value (row data) pairs
 		id: null,
 		title: null,
-		selection: null,  // The whole object (from the JSON server response) that is chosen
+		selection: null,	// The whole object (from the JSON server response) that is chosen
 		route: null,
 		throttle: 150,
 		last_query: null,
-		
+
 		// Init
 		initialize: function () {
 			_.bindAll(this);
 
-			// Get the path to the controller.  If this is not specified via a
+			// Get the path to the controller.	If this is not specified via a
 			// data attribtue of "controller-route" then we attempt to infer it from
 			// the current URL.
 			this.route = this.$el.data('controller-route');
 			if (!this.route) this.route = window.location.pathname;
-			
+
 			// Cache selectors
 			this.$input = this.$('input[type="text"]');
 
@@ -41,8 +42,7 @@ define(function (require) {
 				limit: 15, // Note, this is also enforced in the base controller
 				remote: {
 					url: this.url(),
-					rateLimitWait: this.throttle,
-					wildcard: '%QUERY'
+					rateLimitWait: this.throttle
 				},
 				datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.val); },
 				queryTokenizer: Bloodhound.tokenizers.whitespace
@@ -60,38 +60,38 @@ define(function (require) {
 			});
 
 			// When typeahead is open, listen for selections
-			this.$input.on('typeahead:open', _.bind(function() {
+			this.$input.on('typeahead:opened', _.bind(function() {
 				this.$input.off('input change', this.match);
-				this.$input.on('typeahead:select typeahead:autocomplete', this.match);
+				this.$input.on('typeahead:selected typeahead:autocompleted', this.match);
 			}, this));
 
 			// When it's closed, look for input changes that may invalidate
 			// previous selections
-			this.$input.on('typeahead:close', _.bind(function() {
-				this.$input.off('typeahead:select typeahead:autocomplete', this.match);
+			this.$input.on('typeahead:closed', _.bind(function() {
+				this.$input.off('typeahead:selected typeahead:autocompleted', this.match);
 				this.$input.on('input change', this.match);
 			}, this));
-				
+
 		},
 
-		// Form the URL for the query.  This is in a function so that it can be
+		// Form the URL for the query.	This is in a function so that it can be
 		// subclassed
 		url: function() {
 			return this.route+'/autocomplete?query=%QUERY';
 		},
-		
-		// Callback from after the user inputs anything in the textfield.  Basically,
+
+		// Callback from after the user inputs anything in the textfield.	Basically,
 		// we want to constantly check if what they've entered is valid rather than
 		// rely on bootstrap to tell us.
 		match: function(e, suggestion, dataset) {
-			
+
 			// A suggestion was found
 			if (suggestion) {
 				this.found = true;
 				this.title = suggestion.title;
 				this.id = suggestion.id;
 				this.selection = suggestion;
-				
+
 			// The current input is different than the old one and there
 			// was no suggestion, so wipe it.
 			} else if (this.title != this.$input.val()) {
@@ -100,14 +100,14 @@ define(function (require) {
 			}
 
 		},
-		
-		// Add a new item to the data array. Model is an object like: 
+
+		// Add a new item to the data array. Model is an object like:
 		// {id, title, columns:{}}
 		add:function(model) {
 			this.bloodhound.add(model);
 		}
-		
+
 	});
-	
+
 	return Autocomplete;
 });
