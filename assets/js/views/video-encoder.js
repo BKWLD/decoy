5f2ee41aab2e54ddfefb4a0dd3ccf1856c6e1e7c
Robert Reinhard <robert.reinhard@bkwld.com>
2016-04-26 14:51:04 -0700

    Update the preset when the dropdown is interacted with

--- assets/js/views/video-encoder.js
+++ assets/js/views/video-encoder.js
@@ -1,11 +1,11 @@
 define(function (require) {
-  
+
 	// Dependencies
 	var $ = require('jquery')
 		, _ = require('lodash')
 		, Backbone = require('backbone')
 	;
-	
+
 	// Setup view
 	var View = {};
 	View.initialize = function() {
@@ -19,9 +19,16 @@ define(function (require) {
 		this.$bar = this.$progress.find('.progress-bar');
 		this.$help = this.$('.help-block');
 		this.$currently = this.$help.find('.download, .upload-delete');
+		this.$presetLabel = this.$('.dropdown-toggle .selected');
+		this.$presetValue = this.$('[name^="_preset"]');
+		this.$presetChoices = this.$('.presets a');
 
 		// Start querying for updates
 		if (this.$status.length) this.query();
+
+		// Listen for preset changes
+		this.$presetChoices.on('click', this.onPresetChange);
+		this.updatePresetLabel();
 
 	};
 
@@ -46,7 +53,7 @@ define(function (require) {
 		this.$el.addClass('has-error');
 		this.$file.after('<span class="help-block">'+message+'</span>');
 	};
-	
+
 	// Show the player
 	View.renderPlayer = function(tag) {
 		this.$status.remove();
@@ -59,7 +66,22 @@ define(function (require) {
 	View.renderProgress = function(status, progress) {
 		this.$bar.text(status).css('width', progress+'%');
 	};
-	
+
+	// Handle preset changes
+	View.onPresetChange = function(e) {
+		var preset = $(e.currentTarget).data('val');
+		this.$presetValue.val(preset);
+		this.updatePresetLabel();
+		this.$('.dropdown-toggle').dropdown('toggle');
+	}
+
+	// Update the the preset label using the current value
+	View.updatePresetLabel = function() {
+		var preset = this.$presetValue.val(),
+			title = _.trim(this.$presetChoices.filter('[data-val="'+preset+'"]').text())
+		this.$presetLabel.text(title);
+	};
+
 	// Return view class
 	return Backbone.View.extend(View);
 });
