bf0accb764d72b1f07b2fc706dbc022c236c4ea7
Robert Reinhard <robert.reinhard@bkwld.com>
2016-10-18 17:11:08 -0700

    Fixing pagination
    This broke in Decoy 5 because of changes in Laravel 5
    Fixes #577

--- views/shared/list/_pagination.php
+++ views/shared/list/_pagination.php
@@ -1,10 +1,13 @@
-<?// Sidebar pagination can be found in standard.php?>
+<?
+// Sidebar pagination can be found in standard.php
 
-<?// Standard full list pagination ?>
-<? if ((empty($layout) || $layout == 'full') && method_exists($listing, 'links')): ?>
-	<?=$listing->appends(array(
-		'query' => Input::get('query'),
-		'sort' => Input::get('sort'),
-		'count' => Input::get('count'),
-		))->links(); ?>
-<? endif?>
+// Standard full list pagination
+if ((empty($layout) || $layout == 'full') && method_exists($listing, 'links')) {
+	echo view('decoy::shared.list._paginator', [
+		'paginator' => $listing->appends([
+			'query' => Input::get('query'),
+			'sort' => Input::get('sort'),
+			'count' => Input::get('count'),
+		]),
+	])->render();
+}
--- views/shared/list/_paginator.php
+++ views/shared/list/_paginator.php
@@ -1,52 +1,39 @@
 <?
-
 /**
- * This is implemented by the _pagination partial and is based on 
+ * This is implemented by the _pagination partial and is based on
  * /laravel/framework/src/Illuminate/Pagination/views/slider.php
  * The paginator is only shown if there are more results than the smallest
  * per_page option.
  */
 
+// Deps
+use Bkwld\Decoy\Markup\PaginationPresenter;
+
 // Figure out what controller is handling the request
 $controller = app('decoy.wildcard')->detectController();
 $per_page = $controller::$per_page;
-
-$presenter = new Illuminate\Pagination\BootstrapPresenter($paginator); 
-if ($paginator->getTotal() > $per_page): ?>
+if ($paginator->total() > $per_page): ?>
 	<div class="pagination-wrapper">
-	
+
 		<?// The list of pages ?>
-		<? if ($paginator->getLastPage() > 1): ?>
-			<ul class="pagination pagination-desktop">
-				<?=$presenter->render(); ?>
-			</ul>
-			<? // on mobile, just show first, prev, current, next, last pagination buttons ?>
-			<ul class="pagination pagination-mobile">
-				<?=str_replace('>1<', '>&laquo;<', $presenter->getLink(1)); ?>
-				<?=$presenter->getPrevious('&lsaquo;'); ?>
-				<? // to display the current active page ?>
-				<?=$presenter->getPageRange($paginator->getCurrentPage(),$paginator->getCurrentPage()); ?>
-				<?=$presenter->getNext('&rsaquo;'); ?>
-				<?=str_replace('>'.$paginator->getLastPage().'<', '>&raquo;<', $presenter->getLink($paginator->getLastPage())); ?>
-			</ul>
+		<? if ($paginator->lastPage() > 1): ?>
+			<span class="pagination-desktop">
+				<?= with(new PaginationPresenter($paginator))->render(); ?>
+			</span>
+
+			<?// On mobile, just show first, prev, current, next, last pagination buttons ?>
+			<span class="pagination-mobile">
+				<?= with(new PaginationPresenter($paginator))->renderMobile(); ?>
+			</span>
 		<? endif ?>
-	
-		<?// Per page selector
-		$options = array(
-			$per_page, 
-			$per_page * 2, 
-			'all');
-		$count = Input::get('count', $options[0]); ?>
-		<ul class="per-page pagination">
-			<li class="disabled"><span>Show</span></li>
-			<? foreach($options as $option): ?>
-				<? if ($count == $option): ?>
-					<?=$presenter->getActivePageWrapper(ucfirst($option))?>
-				<? else: ?>
-					<?=$presenter->getPageLinkWrapper($paginator->addQuery('count', $option)->getUrl(1), ucfirst($option))?>
-				<? endif ?>
-			<? endforeach ?>
-	
-		</ul>
+
+		<?// Per page selector ?>
+		<span class="per-page">
+			<?= with(new PaginationPresenter($paginator))->renderPerPageOptions([
+				$per_page,
+				$per_page * 2,
+				'all'
+			]); ?>
+		</span>
 	</div>
 <? endif ?>
