b17286296a4766941311da462d74d2948bc2d17e
Sven Luijten <svenluijten1995@gmail.com>
2017-03-16 22:41:06 +0100

    PSR-2-ify fields folder

--- classes/Fields/Traits/Scopable.php
+++ classes/Fields/Traits/Scopable.php
@@ -1,27 +1,32 @@
-<?php namespace Bkwld\Decoy\Fields\Traits;
+<?php
+
+namespace Bkwld\Decoy\Fields\Traits;
 
 /**
  * Save a scope
  */
-trait Scopable {
+trait Scopable
+{
+    /**
+     * Preserve the scope
+     *
+     * @var callable
+     */
+    private $scope;
 
-	/**
-	 * Preserve the scope
-	 *
-	 * @var callable
-	 */
-	private $scope;
+    /**
+     * Allow the developer to customize the query for related items.  We'll execute the
+     * scope function, passing it a reference to this query to customize
+     *
+     * @param  callable $callback
+     * @return Field    A field
+     */
+    public function scope($callback)
+    {
+        if (is_callable($callback)) {
+            $this->scope = $callback;
+        }
 
-	/**
-	 * Allow the developer to customize the query for related items.  We'll execute the
-	 * scope function, passing it a reference to this query to customize
-	 *
-	 * @param callable $callback
-	 * @return Field A field
-	 */
-	public function scope($callback) {
-		if (is_callable($callback)) $this->scope = $callback;
-		return $this;
-	}
-
-} 
+        return $this;
+    }
+}
--- classes/Fields/Wysiwyg.php
+++ classes/Fields/Wysiwyg.php
@@ -1,6 +1,7 @@
-<?php namespace Bkwld\Decoy\Fields;
+<?php
 
-// Dependencies
+namespace Bkwld\Decoy\Fields;
+
 use Former\Form\Fields\Textarea;
 use Illuminate\Container\Container;
 
@@ -8,22 +9,22 @@ use Illuminate\Container\Container;
  * Essentially a textarea with the wysiwyg class applied, which Decoy JS
  * turns into a Wywsiwyg editor
  */
-class Wysiwyg extends Textarea {
+class Wysiwyg extends Textarea
+{
+    /**
+     * Create a standard and then add the class
+     *
+     * @param Container $app        The Illuminate Container
+     * @param string    $type       text
+     * @param string    $name       Its name
+     * @param string    $label      Its label
+     * @param string    $value      Its value
+     * @param array     $attributes Attributes
+     */
+    public function __construct(Container $app, $type, $name, $label, $value, $attributes)
+    {
+        parent::__construct($app, $type, $name, $label, $value, $attributes);
 
-	/**
-	 * Create a standard and then add the class
-	 *
-	 * @param Container $app        The Illuminate Container
-	 * @param string    $type       text
-	 * @param string    $name       Its name
-	 * @param string    $label      Its label
-	 * @param string    $value      Its value
-	 * @param array     $attributes Attributes
-	 */
-	public function __construct(Container $app, $type, $name, $label, $value, $attributes) {
-		parent::__construct($app, $type, $name, $label, $value, $attributes);
-		$this->addClass('wysiwyg');
-	}
-
-
+        $this->addClass('wysiwyg');
+    }
 }
