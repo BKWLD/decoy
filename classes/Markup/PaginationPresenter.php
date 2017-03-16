2e8b1d01e2f54b78e962bc219e1ea695a3a17609
Sven Luijten <svenluijten1995@gmail.com>
2017-03-16 23:03:08 +0100

    PSR-2-ify files in classes/Markup folder

--- classes/Markup/PaginationPresenter.php
+++ classes/Markup/PaginationPresenter.php
@@ -1,4 +1,6 @@
-<?php namespace Bkwld\Decoy\Markup;
+<?php
+
+namespace Bkwld\Decoy\Markup;
 
 // Deps
 use Illuminate\Pagination\BootstrapThreePresenter;
@@ -7,86 +9,92 @@ use Illuminate\Support\HtmlString;
 /**
  * Extend the bootstrap presetner to provide more customization options
  */
-class PaginationPresenter extends BootstrapThreePresenter {
+class PaginationPresenter extends BootstrapThreePresenter
+{
+    /**
+     * Render just first, prev, current, next, and last pages on mobile
+     *
+     * @return Illuminate\Support\HtmlString
+     */
+    public function renderMobile()
+    {
+        if (!$this->hasPages()) {
+            return '';
+        }
 
-	/**
-	 * Render just first, prev, current, next, and last pages on mobile
-	 *
-	 * @return Illuminate\Support\HtmlString
-	 */
-	public function renderMobile() {
-		if (!$this->hasPages()) return '';
-		return new HtmlString(sprintf(
-			'<ul class="pagination">%s %s %s %s %s</ul>',
-			$this->getFirstPageLink(),
-			$this->getPreviousButton('&lsaquo;'),
-			$this->getActivePageWrapper($this->currentPage(), $this->currentPage()),
-			$this->getNextButton('&rsaquo;'),
-			$this->getLastPageLink()
-		));
-	}
+        return new HtmlString(sprintf(
+            '<ul class="pagination">%s %s %s %s %s</ul>',
+            $this->getFirstPageLink(),
+            $this->getPreviousButton('&lsaquo;'),
+            $this->getActivePageWrapper($this->currentPage(), $this->currentPage()),
+            $this->getNextButton('&rsaquo;'),
+            $this->getLastPageLink()
+        ));
+    }
 
-	/**
-	 * Get first page button
-	 *
-	 * @return string
-	 */
-	protected function getFirstPageLink() {
-		if ($this->currentPage() <= 1) {
-			return $this->getDisabledTextWrapper('&laquo;');
-		} else {
-			return $this->getPageLinkWrapper($this->paginator->url(1), '&laquo;');
-		}
-	}
+    /**
+     * Get first page button
+     *
+     * @return string
+     */
+    protected function getFirstPageLink()
+    {
+        if ($this->currentPage() <= 1) {
+            return $this->getDisabledTextWrapper('&laquo;');
+        } else {
+            return $this->getPageLinkWrapper($this->paginator->url(1), '&laquo;');
+        }
+    }
 
-	/**
-	 * Get last page button
-	 *
-	 * @return string
-	 */
-	protected function getLastPageLink() {
-		if (! $this->paginator->hasMorePages()) {
-			return $this->getDisabledTextWrapper('&raquo;');
-		} else {
-			return $this->getPageLinkWrapper($this->paginator->url(1), '&raquo;');
-		}
-	}
+    /**
+     * Get last page button
+     *
+     * @return string
+     */
+    protected function getLastPageLink()
+    {
+        if (! $this->paginator->hasMorePages()) {
+            return $this->getDisabledTextWrapper('&raquo;');
+        } else {
+            return $this->getPageLinkWrapper($this->paginator->url(1), '&raquo;');
+        }
+    }
 
-	/**
-	 * Render the per page selector links
-	 *
-	 * @param  array $options
-	 * @return Illuminate\Support\HtmlString
-	 */
-	public function renderPerPageOptions($options) {
+    /**
+     * Render the per page selector links
+     *
+     * @param  array                         $options
+     * @return Illuminate\Support\HtmlString
+     */
+    public function renderPerPageOptions($options)
+    {
 
-		// Get the current perpage value
-		$selected = request('count', $options[0]);
+        // Get the current perpage value
+        $selected = request('count', $options[0]);
 
-		// Wrap the links that will be generated in some HTML
-		return new HtmlString(sprintf(
-			'<ul class="pagination">
+        // Wrap the links that will be generated in some HTML
+        return new HtmlString(sprintf(
+            '<ul class="pagination">
 				<li class="disabled"><span>Show</span></li>
 				%s
 			</ul>',
 
-			// Loop through the options and conver to links in the style of the
-			// bootstrap pagination
-			implode('', array_map(function($option) use ($selected) {
+            // Loop through the options and conver to links in the style of the
+            // bootstrap pagination
+            implode('', array_map(function ($option) use ($selected) {
 
-				// Render active state if on selected per page amount
-				if ($selected == $option) {
-					return $this->getActivePageWrapper(
-						ucfirst($option));
+                // Render active state if on selected per page amount
+                if ($selected == $option) {
+                    return $this->getActivePageWrapper(
+                        ucfirst($option));
 
-				// Non-selected link state
-				} else {
-					return $this->getAvailablePageWrapper(
-						$this->paginator->addQuery('count', $option)->url(1),
-						ucfirst($option));
-				}
-			}, $options))
-		));
-	}
-
+                // Non-selected link state
+                } else {
+                    return $this->getAvailablePageWrapper(
+                        $this->paginator->addQuery('count', $option)->url(1),
+                        ucfirst($option));
+                }
+            }, $options))
+        ));
+    }
 }
