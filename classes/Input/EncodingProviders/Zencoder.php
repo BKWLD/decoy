2e547d8429dd5f367a1b1fe0dfb189a475f2c084
Sven Luijten <svenluijten1995@gmail.com>
2017-03-16 22:53:41 +0100

    PSR-2-ify Input directory

--- classes/Input/EncodingProviders/Zencoder.php
+++ classes/Input/EncodingProviders/Zencoder.php
@@ -1,259 +1,276 @@
-<?php namespace Bkwld\Decoy\Input\EncodingProviders;
+<?php
 
-// Dependencies
-use Bkwld\Decoy\Exceptions\Exception;
-use Bkwld\Decoy\Models\Encoding;
+namespace Bkwld\Decoy\Input\EncodingProviders;
+
 use Config;
 use Services_Zencoder;
+use Bkwld\Decoy\Models\Encoding;
 use Services_Zencoder_Exception;
+use Bkwld\Decoy\Exceptions\Exception;
 
 /**
  * Encode videos with Zencoder
  */
-class Zencoder extends EncodingProvider {
+class Zencoder extends EncodingProvider
+{
+    /**
+     * Default outputs configuration
+     *
+     * Regarding internet speeds
+     * @link http://gizmodo.com/americas-internet-inequality-a-map-of-whos-got-the-b-1057686215
+     *
+     * @var array
+     */
+    protected $defaults = [
 
-	/**
-	 * Default outputs configuration
-	 *
-	 * Regarding internet speeds
-	 * http://gizmodo.com/americas-internet-inequality-a-map-of-whos-got-the-b-1057686215
-	 *
-	 * @var array
-	 */
-	protected $defaults = array(
+        // Most everything plays mp4
+        'format' => 'mp4',
 
-		// Most everything plays mp4
-		'format' => 'mp4',
+        // The quailty to encodeat, 1-5
+        // https://app.zencoder.com/docs/api/encoding/rate-control/quality
+        'quality' => 2,
 
-		// The quailty to encodeat, 1-5
-		// https://app.zencoder.com/docs/api/encoding/rate-control/quality
-		'quality' => 2,
+        // Going with main for less CPU intensive decoding
+        'h264_profile' => 'main',
 
-		// Going with main for less CPU intensive decoding
-		'h264_profile' => 'main',
+        // Typically live action content
+        'tuning' => 'film',
 
-		// Typically live action content
-		'tuning' => 'film',
+        // Make the outputs web readable on S3
+        'public' => 1,
 
-		// Make the outputs web readable on S3
-		'public' => 1,
+        // Slower encodes for better quality.  Their docs recommended this
+        // which is why I'm using it instead of "1".
+        'speed' => 2,
 
-		// Slower encodes for better quality.  Their docs recommended this
-		// which is why I'm using it instead of "1".
-		'speed' => 2,
+        // Normalize audio
+        'audio_bitrate' => 56,
+        'audio_sample_rate' => 22050,
+    ];
 
-		// Normalize audio
-		'audio_bitrate' => 56,
-		'audio_sample_rate' => 22050,
-	);
+    /**
+     * Tell the service to encode an asset it's source
+     *
+     * @param  string $source A full URL for the source asset
+     * @param  string $preset The key to the preset function
+     * @return void
+     */
+    public function encode($source, $preset)
+    {
+        // Tell the Zencoder SDK to create a job
+        try {
+            $outputs = $this->outputsConfig($preset);
+            $job = $this->sdk()->jobs->create([
+                'input' => $source,
+                'output' => $outputs,
+            ]);
 
-	/**
-	 * Tell the service to encode an asset it's source
-	 *
-	 * @param string $source A full URL for the source asset
-	 * @param string $preset The key to the preset function
-	 * @return void
-	 */
-	public function encode($source, $preset) {
+            // Store the response from the SDK
+            $this->model->storeJob($job->id, $this->outputsToHash($job->outputs));
 
-		// Tell the Zencoder SDK to create a job
-		try {
-			$outputs = $this->outputsConfig($preset);
-			$job = $this->sdk()->jobs->create(array(
-				'input' => $source,
-				'output' => $outputs,
-			));
+        // Report an error with the encode
+        } catch (Services_Zencoder_Exception $e) {
+            $this->model->status('error', implode(' ', $this->zencoderArray($e->getErrors())));
+        } catch (Exception $e) {
+            $this->model->status('error', $e->getMessage());
+        }
+    }
 
-			// Store the response from the SDK
-			$this->model->storeJob($job->id, $this->outputsToHash($job->outputs));
+    /**
+     * Create the outputs config by merging the `outputs` config of the encode config
+     * file in with $this->defaults and then massaging into Zencoder's expected forat
+     *
+     * @param  string $preset
+     * @return array
+     */
+    protected function outputsConfig($preset)
+    {
+        return $this->addCommonProps(
+            $this->filterHLS(
+                $this->mergeConfigWithDefaults($preset)
+            )
+        );
+    }
 
-		// Report an error with the encode
-		} catch(Services_Zencoder_Exception $e) {
-			$this->model->status('error', implode(' ', $this->zencoderArray($e->getErrors())));
-		} catch(Exception $e) {
-			$this->model->status('error', $e->getMessage());
-		}
+    /**
+     * If the playlist is set to `false` then remove all the HLS encodings.  HLS
+     * is the "http live streaming" outputs that let us serve a video that can adjust
+     * quality levels in response to the bandwidth of the clietn.  Works only on iPhone,
+     * Android, and Safari right now.
+     *
+     * @param  array $config A config assoc array
+     * @return array
+     */
+    protected function filterHLS($config)
+    {
+        // Do not allow any outputs that have a type of "segmented" or "playlist"
+        if (empty($config['playlist'])) {
+            return array_filter($config, function ($output) {
+                return !(isset($output['type']) && in_array($output['type'], ['segmented', 'playlist']));
+            });
+        }
 
-	}
+        // Else, passthrough the config
+        return $config;
+    }
 
-	/**
-	 * Create the outputs config by merging the `outputs` config of the encode config
-	 * file in with $this->defaults and then massaging into Zencoder's expected forat
-	 *
-	 * @param  string $preset
-	 * @return array
-	 */
-	protected function outputsConfig($preset) {
-		return $this->addCommonProps(
-			$this->filterHLS(
-				$this->mergeConfigWithDefaults($preset)
-		));
-	}
+    /**
+     * Update the config with properties that are common to all outputs
+     *
+     * @param  array $config
+     * @return array
+     */
+    protected function addCommonProps($outputs)
+    {
+        // Decoy settings
+        $common = [
 
-	/**
-	 * If the playlist is set to `false` then remove all the HLS encodings.  HLS
-	 * is the "http live streaming" outputs that let us serve a video that can adjust
-	 * quality levels in response to the bandwidth of the clietn.  Works only on iPhone,
-	 * Android, and Safari right now.
-	 *
-	 * @param array $config A config assoc array
-	 * @return array
-	 */
-	protected function filterHLS($config) {
+            // Destination location as a directory
+            'base_url' => $this->destination(),
 
-		// Do not allow any outputs that have a type of "segmented" or "playlist"
-		if (empty($config['playlist'])) return array_filter($config, function($output) {
-			return !(isset($output['type']) && in_array($output['type'], ['segmented', 'playlist']));
-		});
+            // Register for notifications for when the conding is done. If testing
+            // from CLI, just set the app.url config to a ngork domain.
+            'notifications' => [route('decoy::encode@notify')],
 
-		// Else, passthrough the config
-		else return $config;
-	}
+        ];
 
-	/**
-	 * Update the config with properties that are common to all outputs
-	 *
-	 * @param array $config
-	 * @return array
-	 */
-	protected function addCommonProps($outputs) {
+        // Apply common settings ontop of the passed config
+        foreach ($outputs as $label => &$config) {
+            $common['label'] = $label;
 
-		// Decoy settings
-		$common = [
+            // Make the filename from the label
+            $common['filename'] = $label.'.'.$config['format'];
 
-			// Destination location as a directory
-			'base_url' => $this->destination(),
+            // Do the merge
+            $config = array_merge($common, $config);
+        }
 
-			// Register for notifications for when the conding is done. If testing
-			// from CLI, just set the app.url config to a ngork domain.
-			'notifications' => [route('decoy::encode@notify')],
+        // Strip the keys from the array at this point, Zencoder doesn't like them
+        return array_values($outputs);
+    }
 
-		];
+    /**
+     * Massage the outputs from Zencoder into a key-val associative array
+     *
+     * @param  array $outputs
+     * @return array
+     */
+    protected function outputsToHash($outputs)
+    {
+        return array_map(function ($output) {
 
-		// Apply common settings ontop of the passed config
-		foreach($outputs as $label => &$config) {
-			$common['label'] = $label;
+            // If a destination_root was set, subsitute that in for the destination
+            // in the retured URL
+            if ($root = Config::get('decoy.encode.destination_root')) {
+                return str_replace(Config::get('decoy.encode.destination'), $root, $output->url);
+            }
 
-			// Make the filename from the label
-			$common['filename'] = $label.'.'.$config['format'];
+            // Else just return the URL
+            return $output->url;
+        }, $this->zencoderArray($outputs));
+    }
 
-			// Do the merge
-			$config = array_merge($common, $config);
-		}
+    /**
+     * Handle notification requests from the SDK
+     *
+     * @param  array $input Request::input()
+     * @return mixed Reponse to the API
+     */
+    public function handleNotification($input)
+    {
+        // Parse the input
+        $job = $this->sdk()->notifications->parseIncoming()->job;
 
-		// Strip the keys from the array at this point, Zencoder doesn't like them
-		return array_values($outputs);
-	}
+        // Find the encoding model instance.  If it's not found, then just
+        // ignore it.  This can easily happen if someone replaces a video
+        // while one is being uploaded.
+        if (!$model = Encoding::where('job_id', '=', $job->id)->first()) {
+            return;
+        }
 
-	/**
-	 * Massage the outputs from Zencoder into a key-val associative array
-	 *
-	 * @param array $outputs
-	 * @return array
-	 */
-	protected function outputsToHash($outputs) {
-		return array_map(function($output) {
+        // Loop through the jobs and look for error messages.  A job may recieve a
+        // seperate notification for each output that has failed though the job
+        // is still processessing.
+        $errors = trim(implode(' ', array_map(function ($output) {
+            return isset($output->error_message) ? '(Output '.$output->label.') '.$output->error_message : null;
+        }, $this->zencoderArray($job->outputs))));
 
-			// If a destination_root was set, subsitute that in for the destination
-			// in the retured URL
-			if ($root = Config::get('decoy.encode.destination_root')) {
-				return str_replace(Config::get('decoy.encode.destination'), $root, $output->url);
-			}
+        // If there were any messages, treat the job as errored.  This also tries
+        // to fix an issue I saw where a final "error" notifcation wasn't fired even
+        // though multiple jobs failed.
+        $state = empty($errors) ? $job->state : 'failed';
 
-			// Else just return the URL
-			return $output->url;
-		}, $this->zencoderArray($outputs));
-	}
+        // Update the model
+        switch ($state) {
 
-	/**
-	 * Handle notification requests from the SDK
-	 *
-	 * @param array $input Request::input()
-	 * @return mixed Reponse to the API
-	 */
-	public function handleNotification($input) {
+            // Simple passthru of status
+            case 'processing':
+            case 'cancelled':
+                $model->status($job->state);
+                break;
 
-		// Parse the input
-		$job = $this->sdk()->notifications->parseIncoming()->job;
+            // Massage name
+            case 'finished':
+                $model->response = $input;
+                $model->status('complete');
+                break;
 
-		// Find the encoding model instance.  If it's not found, then just
-		// ignore it.  This can easily happen if someone replaces a video
-		// while one is being uploaded.
-		if (!$model = Encoding::where('job_id', '=', $job->id)->first()) return;
+            // Find error messages on the output
+            case 'failed':
+                $model->status('error', $errors);
+                break;
 
-		// Loop through the jobs and look for error messages.  A job may recieve a
-		// seperate notification for each output that has failed though the job
-		// is still processessing.
-		$errors = trim(implode(' ', array_map(function($output) {
-			return isset($output->error_message) ? '(Output '.$output->label.') '.$output->error_message : null;
-		}, $this->zencoderArray($job->outputs))));
+            // Default
+            default:
+                $model->status('error', 'Unkown Zencoder state: '.$job->state);
+        }
+    }
 
-		// If there were any messages, treat the job as errored.  This also tries
-		// to fix an issue I saw where a final "error" notifcation wasn't fired even
-		// though multiple jobs failed.
-		$state = empty($errors) ? $job->state : 'failed';
+    /**
+     * Return the encoding percentage as an int
+     *
+     * @return int 0-100
+     */
+    public function progress()
+    {
+        try {
+            $progress = $this->sdk()->jobs->progress($this->model->job_id);
+            if ($progress->state == 'finished') {
+                return 100;
+            }
 
-		// Update the model
-		switch($state) {
+            return $progress->progress;
+        } catch (\Exception $e) {
+            return 0;
+        }
+    }
 
-			// Simple passthru of status
-			case 'processing';
-			case 'cancelled';
-				$model->status($job->state);
-				break;
+    /**
+     * Build an instance of the SDK
+     *
+     * @return Services_Zencoder
+     */
+    public function sdk()
+    {
+        return new Services_Zencoder(Config::get('decoy.encode.api_key'));
+    }
 
-			// Massage name
-			case 'finished':
-				$model->response = $input;
-				$model->status('complete');
-				break;
+    /**
+     * Convert a Services_Zencoder_Object object to an array
+     *
+     * @param  Services_Zencoder_Object|array $obj
+     * @return array
+     */
+    public function zencoderArray($obj)
+    {
+        if (is_array($obj)) {
+            return $obj;
+        }
 
-			// Find error messages on the output
-			case 'failed':
-				$model->status('error', $errors);
-				break;
+        if (is_a($obj, 'Services_Zencoder_Object')) {
+            return get_object_vars($obj);
+        }
 
-			// Default
-			default:
-				$model->status('error', 'Unkown Zencoder state: '.$job->state);
-		}
-	}
-
-	/**
-	 * Return the encoding percentage as an int
-	 *
-	 * @return int 0-100
-	 */
-	public function progress() {
-		try {
-			$progress = $this->sdk()->jobs->progress($this->model->job_id);
-			if ($progress->state == 'finished') return 100;
-			else return $progress->progress;
-			return $progress;
-		} catch (\Exception $e) {
-			return 0;
-		}
-	}
-
-	/**
-	 * Build an instance of the SDK
-	 *
-	 * @return Services_Zencoder
-	 */
-	public function sdk() {
-		return new Services_Zencoder(Config::get('decoy.encode.api_key'));
-	}
-
-	/**
-	 * Convert a Services_Zencoder_Object object to an array
-	 *
-	 * @param Services_Zencoder_Object|array $obj
-	 * @return array
-	 */
-	public function zencoderArray($obj) {
-		if (is_array($obj)) return $obj;
-		if (is_a($obj, 'Services_Zencoder_Object')) return get_object_vars($obj);
-		throw new Exception('Unexpected object: '.get_class($obj));
-	}
-
+        throw new Exception('Unexpected object: '.get_class($obj));
+    }
 }
--- classes/Input/Search.php
+++ classes/Input/Search.php
@@ -1,196 +1,207 @@
-<?php namespace Bkwld\Decoy\Input;
+<?php
 
-// Dependencies
-use Bkwld\Decoy\Exceptions\Exception;
-use Config;
+namespace Bkwld\Decoy\Input;
+
 use DB;
+use Config;
 use Request;
-use Log;
 use Bkwld\Library\Utils\Text;
+use Bkwld\Decoy\Exceptions\Exception;
 
 /**
  * This class contains logic related to searching from controller
  * index views.  It could have gone in the controller class, but
  * trying to reduce it's bulk
  */
-class Search {
+class Search
+{
+    /**
+     * Utility method to generate a query string that applies the condition
+     * provided in the args
+     *
+     * @param  array  $terms An associative array where the keys are "fields" and the
+     *                       values are "inputs"
+     * @return string
+     */
+    public static function query($terms)
+    {
+        return 'query='.urlencode(json_encode(array_map(function ($input, $field) {
+            return [$field, '=', $input];
+        }, $terms, array_keys($terms))));
+    }
 
-	/**
-	 * Utility method to generate a query string that applies the condition
-	 * provided in the args
-	 *
-	 * @param  array $terms An associative array where the keys are "fields" and the
-	 *                      values are "inputs"
-	 * @return string
-	 */
-	public static function query($terms) {
-		return 'query='.urlencode(json_encode(array_map(function($input, $field) {
-			return [$field, '=', $input];
-		}, $terms, array_keys($terms))));
-	}
+    /**
+     * Apply the effect of a search (which is communicated view Request::get('query'))
+     *
+     * @param  Illuminate\Database\Query\Builder $query
+     * @param  array                             $config Search config from the controller class definition
+     * @return Illuminate\Database\Query\Builder
+     */
+    public function apply($query, $config)
+    {
+        // Do nothing if no query in the input
+        if (!Request::has('query')) {
+            return $query;
+        }
 
-	/**
-	 * Apply the effect of a search (which is communicated view Request::get('query'))
-	 *
-	 * @param  Illuminate\Database\Query\Builder $query
-	 * @param  array $config Search config from the controller class definition
-	 * @return Illuminate\Database\Query\Builder
-	 */
-	public function apply($query, $config) {
+        // Expand the config
+        $config = $this->longhand($config);
 
-		// Do nothing if no query in the input
-		if (!Request::has('query')) return $query;
+        // Deserialize the query and loop through
+        $conditions = json_decode(Request::get('query'));
+        if (!is_array($conditions)) {
+            throw new Exception('Bad query');
+        }
 
-		// Expand the config
-		$config = $this->longhand($config);
+        foreach ($conditions as $condition) {
 
-		// Deserialize the query and loop through
-		$conditions = json_decode(Request::get('query'));
-		if (!is_array($conditions)) throw new Exception('Bad query');
-		foreach($conditions as $condition) {
+            // Get the field name by taking the index and looking up which key it corresponds to
+            $field = $condition[0];
+            $field_config = $config[$field];
 
-			// Get the field name by taking the index and looking up which key it corresponds to
-			$field = $condition[0];
-			$field_config = $config[$field];
+            // Extract vars for query
+            $comparison = $condition[1];
+            $input = $condition[2];
 
-			// Extract vars for query
-			$comparison = $condition[1];
-			$input = $condition[2];
+            // Use an app-defined query ...
+            if (isset($config[$field]['query'])) {
+                call_user_func($config[$field]['query'], $query, $comparison, $input);
 
-			// Use an app-defined query ...
-			if (isset($config[$field]['query'])) {
-				call_user_func($config[$field]['query'], $query, $comparison, $input);
+            // ... or one of the simple, standard ones
+            } else {
+                $this->condition($query, $field, $comparison, $input, $config[$field]['type']);
+            }
+        }
 
-			// ... or one of the simple, standard ones
-			} else $this->condition($query, $field, $comparison, $input, $config[$field]['type']);
+        // Return the agumented query
+        return $query;
+    }
 
-		}
+    /**
+     * Add a condition to a query
+     *
+     * @param  Illuminate\Database\Query\Builder $query
+     * @param  string                            $field      The field name from search config
+     * @param  string                            $comparison The operator string from the search UI
+     * @param  string                            $input      The input for the field
+     * @param  string                            $type       The type of the field
+     * @return Illuminate\Database\Query\Builder
+     */
+    private function condition($query, $field, $comparison, $input, $type)
+    {
+        // Convert date formats
+        if ($type == 'date') {
+            $field = DB::raw("DATE($field)");
+            $input = date('Y-m-d', strtotime($input));
+        }
 
-		// Return the agumented query
-		return $query;
+        // Apply the where
+        switch ($comparison) {
 
-	}
+            // NULL safe equals and not equals
+            // http://stackoverflow.com/a/19778341/59160
+            case '=': return $query->whereRaw(sprintf('%s <=> %s',
+                is_string($field) ? "`{$field}`" : $field,
+                $input == '' ? 'NULL' : DB::connection()->getPdo()->quote($input)));
+            case '!=': return $query->whereRaw(sprintf('NOT(%s <=> %s)',
+                is_string($field) ? "`{$field}`" : $field,
+                $input == '' ? 'NULL' : DB::connection()->getPdo()->quote($input)));
 
-	/**
-	 * Add a condition to a query
-	 *
-	 * @param  Illuminate\Database\Query\Builder $query
-	 * @param  string $field The field name from search config
-	 * @param  string $comparison The operator string from the search UI
-	 * @param  string $input The input for the field
-	 * @param  string $type The type of the field
-	 * @return Illuminate\Database\Query\Builder
-	 */
-	private function condition($query, $field, $comparison, $input, $type) {
+            // Not Like
+            case '!%*%':
+                $comparison = substr($comparison, 1);
+                $input = str_replace('*', $input, $comparison);
 
-		// Convert date formats
-		if ($type == 'date') {
-			$field = DB::raw("DATE($field)");
-			$input = date('Y-m-d', strtotime($input));
-		}
+                return $query->where($field, 'NOT LIKE', $input);
 
-		// Apply the where
-		switch ($comparison) {
+            // Like
+            case '*%':
+            case '%*':
+            case '%*%':
+                $input = str_replace('*', $input, $comparison);
 
-			// NULL safe equals and not equals
-			// http://stackoverflow.com/a/19778341/59160
-			case '=': return $query->whereRaw(sprintf('%s <=> %s',
-				is_string($field) ? "`{$field}`" : $field,
-				$input == '' ? 'NULL' : DB::connection()->getPdo()->quote($input)));
-			case '!=': return $query->whereRaw(sprintf('NOT(%s <=> %s)',
-				is_string($field) ? "`{$field}`" : $field,
-				$input == '' ? 'NULL' : DB::connection()->getPdo()->quote($input)));
+                return $query->where($field, 'LIKE', $input);
 
-			// Not Like
-			case '!%*%':
-				$comparison = substr($comparison, 1);
-				$input = str_replace('*', $input, $comparison);
-				return $query->where($field, 'NOT LIKE', $input);
+            // Defaults
+            default:
+                return $query->where($field, $comparison, $input);
+        }
+    }
 
-			// Like
-			case '*%':
-			case '%*':
-			case '%*%':
-				$input = str_replace('*', $input, $comparison);
-				return $query->where($field, 'LIKE', $input);
+    /**
+     * Make the shorthand options of the search config explicit
+     *
+     * @param  array $config Search config from the controller class definition
+     * @return array
+     */
+    public function longhand($config)
+    {
+        $search = [];
+        foreach ($config as $key => $val) {
 
-			// Defaults
-			default:
-				return $query->where($field, $comparison, $input);
-		}
-	}
+            // Make locale menu
+            if ($val == 'locale') {
+                $search['locale'] = [
+                    'type' => 'select',
+                    'label' => 'Locale',
+                    'options' => Config::get('decoy.site.locales'),
+                ];
 
-	/**
-	 * Make the shorthand options of the search config explicit
-	 *
-	 * @param  array $config Search config from the controller class definition
-	 * @return array
-	 */
-	public function longhand($config) {
-		$search = array();
-		foreach($config as $key => $val) {
+            // Not associative assume it's a text field
+            } elseif (is_numeric($key)) {
+                $search[$val] = ['type' => 'text', 'label' => Text::titleFromKey($val)];
 
-			// Make locale menu
-			if ($val == 'locale') {
-				$search['locale'] = [
-					'type' => 'select',
-					'label' => 'Locale',
-					'options' => Config::get('decoy.site.locales'),
-				];
+            // If value isn't an array, make a default label
+            } elseif (!is_array($val)) {
+                $search[$key] = ['type' => $val, 'label' => Text::titleFromKey($key)];
 
-			// Not associative assume it's a text field
-			} else if (is_numeric($key)) {
-				$search[$val] = array('type' => 'text', 'label' => Text::titleFromKey($val));
+            // Add the meta array
+            } else {
 
-			// If value isn't an array, make a default label
-			} else if (!is_array($val)) {
-				$search[$key] = array('type' => $val, 'label' => Text::titleFromKey($key));
+                // Make a default label
+                if (empty($val['label'])) {
+                    $val['label'] = Text::titleFromKey($key);
+                }
 
-			// Add the meta array
-			} else {
+                // Support class static method or variable as options for a select
+                if (!empty($val['type'])
+                    && $val['type'] == 'select'
+                    && !empty($val['options'])
+                    && is_string($val['options'])) {
+                    $val['options'] = $this->longhandOptions($val['options']);
+                }
 
-				// Make a default label
-				if (empty($val['label'])) $val['label'] = Text::titleFromKey($key);
+                // Apply the meta data
+                $search[$key] = $val;
+            }
+        }
 
-				// Support class static method or variable as options for a select
-				if (!empty($val['type'])
-					&& $val['type'] == 'select'
-					&& !empty($val['options'])
-					&& is_string($val['options'])) {
-					$val['options'] = $this->longhandOptions($val['options']);
-				}
+        return $search;
+    }
 
-				// Apply the meta data
-				$search[$key] = $val;
-			}
+    /**
+     * Parse select options, returning a transformed array with static arrays
+     * or callbacks executed
+     *
+     * @param  array $options
+     * @return array
+     */
+    private function longhandOptions($options)
+    {
+        // Call static method.  You don't pass the paranethesis
+        // to static calls
+        if (preg_match('#::.+\(\)#', $options)) {
+            return call_user_func(substr($options, 0, strlen($options) - 2));
+        }
 
-		}
-		return $search;
-	}
+        // Return static variable
+        if (preg_match('#::\$#', $options)) {
+            list($class, $var) = explode('::$', $options);
 
-	/**
-	 * Parse select options, returning a transformed array with static arrays
-	 * or callbacks executed
-	 *
-	 * @param  array $options
-	 * @return array
-	 */
-	private function longhandOptions($options) {
+            return $class::$$var;
+        }
 
-		// Call static method.  You don't pass the paranethesis
-		// to static calls
-		if (preg_match('#::.+\(\)#', $options)) {
-			return call_user_func(substr($options, 0, strlen($options) - 2));
-
-		// Return static variable
-		} else if (preg_match('#::\$#', $options)) {
-			list($class, $var) = explode('::$', $options);
-			return $class::$$var;
-
-		// Unknown format
-		} else throw new Exception('Could not parse option: '.$options);
-
-	}
-
-
+        // Unknown format
+        throw new Exception('Could not parse option: '.$options);
+    }
 }
