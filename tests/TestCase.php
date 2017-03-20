f073480777fbfc72a9267e1216ec8558746bf9ce
Robert Reinhard <robert.reinhard@bkwld.com>
2017-03-20 16:49:43 -0700

    Add Mockery cleanup trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,12 @@ namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as LaravelTestCase;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
 abstract class TestCase extends LaravelTestCase
 {
+    use MockeryPHPUnitIntegration; // Increments assertion count
+
     /**
      * The base URL to use while testing the application.
      *
