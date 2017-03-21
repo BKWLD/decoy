00ec5005f3235ca5cb450924cfc2bbc7d9778db3
Michael Starks <michael.starks@bkwld.com>
2017-03-21 16:49:12 -0700

    adjusted example article migration for tests

--- example/database/migrations/2017_03_21_032710_create_articles.php
+++ example/database/migrations/2017_03_21_032710_create_articles.php
@@ -18,10 +18,10 @@ class CreateArticles extends Migration
             $table->string('category')->index();
             $table->string('title');
             $table->string('slug')->unique();
-            $table->text('body');
+            $table->text('body')->nullable();
 
             $table->date('date')->index();
-            $table->boolean('featured')->index();
+            $table->boolean('featured')->index()->nullable();
             $table->integer('position');
             $table->boolean('public');
             $table->timestamps();
