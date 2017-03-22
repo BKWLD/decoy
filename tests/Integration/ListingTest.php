8931f8d706088e9b234e9975e0d63b22914ed103
Michael Starks <michael.starks@bkwld.com>
2017-03-22 15:50:52 -0700

    Adding more tests to ListingTest

--- tests/Integration/ListingTest.php
+++ tests/Integration/ListingTest.php
@@ -30,13 +30,71 @@ class ListingTest extends TestCase
 
         list($first_article, $second_article) = $articles;
 
-        $this->json('PUT', 'admin/articles/' . $first_article->id, [
+        $response = $this->json('PUT', 'admin/articles/' . $first_article->id, [
             'id' => $first_article->id,
             'position' => 2,
             'public' => true
+        ], [
+            'X-Requested-With' => 'XMLHttpRequest',
         ]);
 
         $this->assertEquals(2, $first_article->fresh()->position);
+    }
+
+    /**
+     * Test deleting from the listing view
+     * @return void
+     */
+    public function testListingDestroy()
+    {
+        $this->auth();
+        $article = factory(Article::class)->create();
+
+        $response = $this->call('DELETE', 'admin/articles/' . $article->id);
+
+        $this->assertEmpty($article->fresh());
+    }
+
+    /**
+     * Test if making the listing item public is working
+     * @return void
+     */
+    public function testListingPublic()
+    {
+        $this->auth();
+        $article = factory(Article::class)->create([
+            'public' => 0
+        ]);
+
+        $response = $this->json('PUT', 'admin/articles/' . $article->id, [
+            'id' => 1,
+            'position' => 1,
+            'public' => true
+        ], [
+            'X-Requested-With' => 'XMLHttpRequest',
+        ]);
+
+        $this->assertEquals(1, $article->fresh()->public);
+    }
+
+    /**
+     * Test if making the listing item private is working
+     * @return void
+     */
+    public function testListingPrivate()
+    {
+        $this->auth();
+        $article = factory(Article::class)->create();
+
+        $response = $this->json('PUT', 'admin/articles/' . $article->id, [
+            'id' => 1,
+            'position' => 1,
+            'public' => false
+        ], [
+            'X-Requested-With' => 'XMLHttpRequest',
+        ]);
+
+        $this->assertEquals(0, $article->fresh()->public);
     }
 
 }
