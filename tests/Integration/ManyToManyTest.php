698f54126368b8d64c680d29a56fc22aaa0d1034
Robert Reinhard <robert.reinhard@bkwld.com>
2017-03-23 09:40:32 -0700

    Add tests for many to many checklist field

--- tests/Integration/ManyToManyTest.php
+++ tests/Integration/ManyToManyTest.php
@@ -73,9 +73,14 @@ class ManyToManyTest extends TestCase
 
     /**
      * Test detaching (deleting) of a many to many relationship from the sidebar
+     *
+     * @return void
      */
     public function testSidebarDetach()
     {
+        $this->tag->articles()->attach($this->article->id);
+        $this->assertEquals(1, $this->article->tags()->count());
+
         $this->delete('admin/admin/articles/1/remove', [
             'parent_controller' => 'App\Http\Controllers\Admin\Tags',
             'parent_id' => $this->tag->id,
@@ -85,16 +90,55 @@ class ManyToManyTest extends TestCase
 
     /**
      * Test bulk detaching of a many to many relationship from the sidebar
+     *
+     * @return void
      */
     public function testSidebarBulkDetach()
     {
         $article = factory(Article::class)->create();
+        $this->tag->articles()->attach([$article->id, $this->article->id]);
+        $this->assertEquals(2, $this->tag->articles()->count());
+
         $this->delete('admin/admin/articles/1/remove', [
             'parent_controller' => 'App\Http\Controllers\Admin\Tags',
             'parent_id' => $this->tag->id,
             'ids' => $this->article->id.','.$article->id,
         ], $this->ajaxHeader());
+
         $this->assertEquals(0, $this->tag->articles()->count());
     }
+
+    /**
+     * Test adding relations via the many to many checklist field
+     *
+     * @return void
+     */
+    public function testManyToManyChecklistAttach()
+    {
+        $this->post('admin/articles/'.$this->article->id.'/edit', [
+            '_many_to_many_tags' => [ $this->tag->id ],
+        ]);
+
+        $this->assertEquals(1, $this->article->tags()->count());
+        $this->assertEquals($this->tag->id, $this->article->tags()->first()->id);
+    }
+
+    /**
+     * Test removing relations via the many to many checklist field
+     *
+     * @return void
+     */
+    public function testManyToManyChecklistDetach()
+    {
+        $this->article->tags()->attach($this->tag->id);
+        $this->assertEquals(1, $this->article->tags()->count());
+
+        $this->post('admin/articles/'.$this->article->id.'/edit', [
+            '_many_to_many_tags' => [ ],
+        ]);
+
+        $this->assertEquals(0, $this->article->tags()->count());
+    }
+
 
 }
