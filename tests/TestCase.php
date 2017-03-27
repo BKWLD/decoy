d5da7ab62c49ec151e8f80d71d01bd95ffc5c1e3
Robert Reinhard <robert.reinhard@bkwld.com>
2017-03-27 15:46:22 -0700

    Using a mocked disk during integration tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,10 @@ use Bkwld\Decoy\Models\Admin;
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as LaravelTestCase;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Vfs\VfsAdapter;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use VirtualFileSystem\FileSystem as Vfs;
 
 abstract class TestCase extends LaravelTestCase
 {
@@ -18,6 +21,24 @@ abstract class TestCase extends LaravelTestCase
      * @var string
      */
     protected $baseUrl = 'http://localhost';
+
+    /**
+     * Flysystem disk that Decoy will write to
+     *
+     * @var Flysystem
+     */
+    protected $disk;
+
+    /**
+     * Common setUp tasks
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->mockDisk();
+    }
 
     /**
      * Creates the application.
@@ -56,5 +77,18 @@ abstract class TestCase extends LaravelTestCase
     protected function ajaxHeader()
     {
         return [ 'X-Requested-With' => 'XMLHttpRequest' ];
+    }
+
+    /**
+     * Create a fake disk for filesystem tests and override the uphuck disk
+     * with it so no actual files get written to the filesystem.
+     *
+     * @return
+     */
+    protected function mockDisk()
+    {
+        $this->app->singleton('upchuck.disk', function($app) {
+            return $this->disk = new Filesystem(new VfsAdapter(new Vfs));
+        });
     }
 }
