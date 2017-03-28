4269e971a0070c61dd71c08b617278e73d41b283
Robert Reinhard <robert.reinhard@bkwld.com>
2017-03-28 11:07:41 -0700

    Rewrap comments

--- classes/Collections/Elements.php
+++ classes/Collections/Elements.php
@@ -125,16 +125,16 @@ class Elements extends Collection
     }
 
     /**
-     * Populate the Collection from the Cache or build the cache if it isn't set yet.
-     * But only if not on a local environment (as new Elements are added, you would have
-     * to keep re-clearing the cache)
+     * Populate the Collection from the Cache or build the cache if it isn't set
+     * yet. But only if not on a local environment (as new Elements are added,
+     * you would have to keep re-clearing the cache)
      *
      * @return $this
      */
     public function hydrate($include_extra = false)
     {
-        // If including extra YAML config vars, neither use the cache NOR allow the cache
-        // to be saved with it
+        // If including extra YAML config vars, neither use the cache NOR allow
+        // the cache to be saved with it
         if ($include_extra && !$this->has_extra) {
             $this->has_extra = true;
             $this->items = $this->mergeSources();
