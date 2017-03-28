e1b51fdc0f077157fd65072360423909305dee35
Michael Starks <michael.starks@bkwld.com>
2017-03-28 14:03:57 -0700

    created filetests for handling of files/images

--- tests/Integration/FileTest.php
+++ tests/Integration/FileTest.php
@@ -2,6 +2,7 @@
 namespace Tests\Integration;
 
 use App\Article;
+use App\Recipe;
 use Tests\TestCase;
 use Illuminate\Http\UploadedFile;
 
@@ -9,7 +10,17 @@ class FileTest extends TestCase
 {
 
     /**
-     * Create data used for crud tests
+     * Common init
+     *
+     * @return void
+     */
+    protected function setUp() {
+        parent::setUp();
+        $this->auth();
+    }
+
+    /**
+     * Create data used for image upload test
      *
      * @return array
      */
@@ -62,13 +73,49 @@ class FileTest extends TestCase
     }
 
     /**
+     * Create data used for file upload test
+     *
+     * @return array
+     */
+    public function createRecipeData()
+    {
+        $tmp_dir = ini_get('upload_tmp_dir') ?: sys_get_temp_dir();
+        $file_name = 'file.txt';
+        $file_path = $tmp_dir.'/'.$file_name;
+        if (!file_exists($file_path)) {
+            file_put_contents($file_path, 'test');
+        }
+
+        return [
+
+            // Params
+            [
+                'title' => 'Test Recipe',
+                'locale' => 'en',
+                'public' => 1,
+            ],
+
+            // Files
+            [
+                'file' => new UploadedFile(
+                    $file_path,
+                    $file_name,
+                    null,
+                    null,
+                    null,
+                    true
+                )
+            ],
+        ];
+    }
+
+    /**
      * Test the image file field uploads and is stored when saved
      *
      * @return void
      */
-    public function testImageStore()
+    public function testImageUploadStore()
     {
-        $this->auth();
         list($params, $files) = $this->createData();
 
         $response = $this->call('POST', 'admin/articles/create', array_merge($params, [
@@ -82,9 +129,100 @@ class FileTest extends TestCase
     /**
      * Test that the upload file field gets stored when saved
      */
-    public function testUploadStore()
+    public function testFileUploadStore()
     {
-        
+        list($params, $files) = $this->createRecipeData();
+
+        $response = $this->call('POST', 'admin/recipes/create', array_merge($params, [
+            '_save' => 'save',
+        ]), [], $files);
+
+        $recipe = Recipe::first();
+        $this->assertEquals('Test Recipe', $recipe->title);
+        $path = app('upchuck')->path($recipe->file);
+        $this->assertTrue($this->disk->has($path));
+    }
+
+    /**
+     * Test that an image gets removed when the article is deleted
+     *
+     * @return void
+     */
+    public function testImageRemovedOnDelete()
+    {
+        // Make image
+        $img = imagecreatetruecolor(20, 20);
+        ob_start();
+        imagejpeg($img);
+        $this->disk->put('test.jpg', ob_get_clean());
+        imagedestroy($img);
+
+        // Create recipe with file attachments
+        $article = factory(Article::class)->create();
+        $article->images()->create([
+            'file' => '/uploads/test.jpg',
+            'file_type' => 'image/jpeg',
+            'file_size' => 10,
+            'width' => 20,
+            'height' => 20,
+        ]);
+
+        // Test first that the image is actually there
+        $path = app('upchuck')->path($article->img()->file);
+        $this->assertNotEmpty($article);
+        $this->assertTrue($this->disk->has($path));
+
+        $response = $this->get('admin/articles/'.$article->id.'/destroy');
+
+        // Test that article and the image are both removed
+        $this->assertEmpty($article->fresh());
+        $this->assertEmpty($this->disk->has($path));
+    }
+
+    /**
+     * Test that a file gets removed when the parent is deleted
+     *
+     * @return void
+     */
+    public function testFileRemovedOnDelete()
+    {
+        $this->disk->put('test.txt', 'test');
+
+        $recipe = factory(Recipe::class)->create([
+            'file' => '/uploads/test.txt'
+        ]);
+
+        $response = $this->get('admin/recipes/'.$recipe->id.'/destroy');
+
+        $path = app('upchuck')->path($recipe->file);
+        $this->assertEmpty($recipe->fresh());
+        $this->assertFalse($this->disk->has($path));
+    }
+
+    /**
+     * Test that a file is deleted if checkbox is checked on save
+     *
+     * @return void
+     */
+    public function testFileRemovedOnSave()
+    {
+        $this->disk->put('test.txt', 'test');
+
+        $recipe = factory(Recipe::class)->create([
+            'file' => '/uploads/test.txt'
+        ]);
+
+        $this->assertNotEmpty($recipe->file);
+
+        $response = $this->post('admin/recipes/'.$recipe->id.'/edit', [
+            'file' => '',
+            '_save' => 'save',
+        ]);
+
+        $path = app('upchuck')->path($recipe->file);
+        $this->assertNotEmpty($recipe->fresh());
+        $this->assertEmpty($recipe->fresh()->file);
+        $this->assertFalse($this->disk->has($path));
     }
 
 }
