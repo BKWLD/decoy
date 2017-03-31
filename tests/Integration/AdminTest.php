db5e7eade9d08d879c423c3d0da2ce21ca2d045a
Michael Starks <michael.starks@bkwld.com>
2017-03-31 11:51:19 -0700

    Added tests for the password reset flow
    [touch:26]

--- tests/Integration/AdminTest.php
+++ tests/Integration/AdminTest.php
@@ -2,8 +2,10 @@
 namespace Tests\Integration;
 
 use App\Article;
-use Tests\TestCase;
 use Bkwld\Decoy\Models\Admin;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Tests\TestCase;
 
 class AdminTest extends TestCase
 {
@@ -65,6 +67,81 @@ class AdminTest extends TestCase
 
         $response = $this->get('admin/admins/2/disable');
         $this->assertResponseStatus(403);
+    }
+
+    /**
+     * Test the reset password flow
+     *
+     * @return void
+     */
+    public function testResetPasswordIndex()
+    {
+        $response = $this->get('admin/forgot');
+
+        $this->assertResponseOk();
+    }
+
+    /**
+     * Test the reset password submit button works
+     *
+     * @return void
+     */
+    public function testResetPasswordSubmit()
+    {
+        $response = $this->call('POST', 'admin/forgot', [
+            'email' => 'test@domain.com',
+        ]);
+
+        $this->assertResponseStatus(302);
+    }
+
+    /**
+     * Test reset password form
+     *
+     * @return void
+     */
+    public function testResetPasswordFormIndex()
+    {
+        $token = Str::random(60);
+        \DB::table('password_resets')->insert([
+            'email' => 'test@domain.com',
+            'token' => $token,
+            'created_at' => Carbon::now(),
+        ]);
+
+        $response = $this->get('admin/reset/'.$token);
+        $this->assertResponseOk();
+    }
+
+    /**
+     * Test that the reset password form works
+     *
+     * @return void
+     */
+    public function testResetPasswordFormSave()
+    {
+        $current_password = Admin::findOrFail(1)->password;
+
+        $token = Str::random(60);
+        \DB::table('password_resets')->insert([
+            'email' => 'test@domain.com',
+            'token' => $token,
+            'created_at' => Carbon::now(),
+        ]);
+
+        $response = $this->post('admin/reset/'.$token, [
+            'email' => 'test@domain.com',
+            'password' => 'farting',
+            'password_confirmation' => 'farting',
+            'token' => $token,
+        ]);
+
+        $new_password = Admin::findOrFail(1)->password;
+
+        $this->assertResponseStatus(302);
+        $this->assertNotEquals($current_password, $new_password);
+        $this->assertEmpty(\DB::table('password_resets')
+            ->where('email', 'test@domain.com')->get());
     }
 
 }
