a4ee25fd891375995b883fef3d31352fe3735b5c
Robert Reinhard <robert.reinhard@bukwild.com>
2017-08-23 12:39:23 -0700

    Fixing commands title
    Closes #78

--- lang/en/commands.php
+++ lang/en/commands.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'controller.title' => 'Admins',
+    'controller.title' => 'Commands',
     'controller.description' => 'Trigger any command for this site. Note: these may take awhile to execute.',
 
     'execute' => 'Execute',
