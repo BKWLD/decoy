72da336847c2426e942e284edb82a5e3c134da4f
Robert Reinhard <robert.reinhard@bkwld.com>
2017-11-06 11:21:31 -0800

    Vertical aligning listing actions icons
    Closes #85

--- example/public/assets/decoy/index.js
+++ example/public/assets/decoy/index.js
@@ -1,4 +1,4 @@
-/*! 📝 Bukwild 💾 10.10.17 👍 */
+/*! 📝 Bukwild 💾 11.6.17 👍 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
