a61d4d7dfc2538cf64a507f8dbea9669d8364392
Robert Reinhard <robert.reinhard@bkwld.com>
2017-11-29 09:14:52 -0800

    Add tests of Change edit

--- tests/Integration/ChangesTest.php
+++ tests/Integration/ChangesTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Tests\Integration;
 
+use App\Tag;
+use Bkwld\Decoy\Models\Change;
 use Cache;
 use Carbon\Carbon;
 use Decoy;
@@ -10,6 +12,13 @@ class ChangesTest extends TestCase
 {
 
     /**
+     * The tag that triggreed changes
+     *
+     * @var Tag
+     */
+    protected $tag;
+
+    /**
      * Common init
      *
      * @return void
@@ -17,6 +26,13 @@ class ChangesTest extends TestCase
     protected function setUp() {
         parent::setUp();
         $this->auth();
+
+        // Create a series of Change records through messing with the soft
+        // deleting Tag model
+        $this->tag = factory(Tag::class)->create([ 'name' => 'Name' ]);
+        $this->tag->name = 'New name';
+        $this->tag->save();
+        $this->tag->delete();
     }
 
     /**
@@ -24,10 +40,45 @@ class ChangesTest extends TestCase
      *
      * @return void
      */
-    public function testChangesIndex()
+    public function testIndex()
     {
         $response = $this->get('admin/changes');
         $response->assertStatus(200);
+
+        // Test that all the changes from the setUp were created
+        $this->assertEquals(3, Change::count());
+    }
+
+    /**
+     * Test that the edit response returns JSON
+     *
+     * @return void
+     */
+    public function testCreatedEdit()
+    {
+        $response = $this->get('admin/changes/1/edit');
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'title' => 'Name',
+                'action' => 'created',
+            ]);
+    }
+
+    /**
+     * Test that the edit response returns JSON
+     *
+     * @return void
+     */
+    public function testUpdatedEdit()
+    {
+        $response = $this->get('admin/changes/2/edit');
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'title' => 'New name',
+                'action' => 'updated',
+            ]);
     }
 
 
